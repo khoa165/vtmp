120a14c679ecff6c272ff2cf54f1fad8638c7aeb
Minh Q Nguyen <116717692+QuangMinhNguyen27405@users.noreply.github.com>
2025-03-25 00:23:42 -0400

    Create application and interview data model (#35)
    Co-authored-by: dsmai <son.dmai@gmail.com>

---
+++ web/src/models/application.model.ts
@@ -0,0 +1,55 @@
+import mongoose, { Document } from 'mongoose';
+
+enum ApplicationStatus {
+  SUBMITTED = 'SUBMITTED',
+  IN_PROGRESS = 'IN_PROGRESS',
+  OFFER = 'OFFER',
+  REJECTED = 'REJECTED',
+  OA_RECEIVED = 'OA_RECEIVED',
+  WITHDRAWN = 'WITHDRAWN',
+}
+
+interface IApplication extends Document {
+  jobPostingId: mongoose.Schema.Types.ObjectId;
+  userId: mongoose.Schema.Types.ObjectId;
+  hasApplied: boolean;
+  status: ApplicationStatus;
+  appliedOnDate: Date;
+  note?: string;
+}
+
+const ApplicationSchema = new mongoose.Schema<IApplication>(
+  {
+    jobPostingId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'JobPosting',
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    hasApplied: {
+      type: Boolean,
+      default: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(ApplicationStatus),
+      default: ApplicationStatus.SUBMITTED,
+    },
+    appliedOnDate: {
+      type: Date,
+      default: Date.now,
+    },
+    note: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IApplication>('Application', ApplicationSchema);
