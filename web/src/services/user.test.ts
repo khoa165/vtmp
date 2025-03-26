96ba3fce6e7d2e3c618e85805f377208c5030c5f
Nam Nguyen <nguy1053@msu.edu>
2025-03-26 11:11:06 -0400

    Setup tests for User Service login

--- web/src/services/user.test.ts
+++ web/src/services/user.test.ts
@@ -1,1 +1,15 @@
+import { describe } from 'mocha';
 
+describe('User Service', () => {
+  describe('Login', () => {
+    it('should throw error user not found', () => {});
+
+    it('should throw error wrong password', () => {});
+
+    it('should login successfully and return valid jwt token', () => {});
+
+    it('should login successfully and return token with correct user information', () => {
+        
+    })
+  });
+});
