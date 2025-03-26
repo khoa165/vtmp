0e85c8d008e31c09c615f8e03e08c3dd41453c74
nickbar01234 <nickbar01234gmail.com>
2025-03-26 02:38:32 -0400

    Bump @types/react

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -3,7 +3,6 @@ import { StatsigProvider } from 'statsig-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { App } from 'components/app';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <StatsigProvider
