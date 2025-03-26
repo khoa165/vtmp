02c769b4eef4d4f1e6c93aea8bf071fd4fc7de22
Nam Nguyen <nguy1053@msu.edu>
2025-03-26 11:09:17 -0400

    Create UserService login function

--- web/src/services/user.service.ts
+++ web/src/services/user.service.ts
@@ -1,17 +1,27 @@
+import { getConfig } from '@/config/config';
 import UserRepository from '@/repositories/user.repository';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
+const config = getConfig();
+
 const UserService = {
   login: async ({ email, password }: { email: string; password: string }) => {
     const user = await UserRepository.findByEmail(email);
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      throw new Error('Invalid credentials');
+    if (!user) {
+      throw new Error('User not found');
+    }
+    const passwordMatched = await bcrypt.compare(
+      password,
+      user.encryptedPassword ?? ''
+    );
+    if (!passwordMatched) {
+      throw new Error('Wrong password');
     }
 
     const token = jwt.sign(
       { id: user._id.toString(), email: user.email },
-      process.env.JWT_SECRET ?? 'vtmp-secret',
+      config.JWT_SECRET,
       { expiresIn: '1h' }
     );
     return token;
