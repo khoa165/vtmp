274a76155d19613d3a628c800dfb49ab9c3b5d94
Nam Nguyen <nguy1053@msu.edu>
2025-03-26 19:26:58 -0400

    Create login controller and move from UserController to AuthController

--- web/src/controllers/auth.controller.ts
+++ web/src/controllers/auth.controller.ts
@@ -2,7 +2,21 @@ import AuthService from '@/services/auth.service';
 import { Request, Response } from 'express';
 
 const AuthController = {
-  login: async (req: Request, res: Response) => {},
+  /// PARSING, VALIDATING, RESPONSE
+  login: async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        res.status(400).json({ message: 'Missing email or password' });
+      }
+
+      const token = await AuthService.login(req.body);
+      res.status(200).json({ token });
+    } catch (error: any) {
+      const errorMessage = error?.message ?? 'An error occurred';
+      res.status(401).json({ message: errorMessage });
+    }
+  },
 };
 
 export default AuthController;
