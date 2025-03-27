07acce0fea3ff7340b407176b327f701d067676a
Nam Nguyen <nguy1053@msu.edu>
2025-03-26 20:59:04 -0400

    Update Auth controller login to seperate missing email and missing password errors

--- web/src/controllers/auth.controller.ts
+++ web/src/controllers/auth.controller.ts
@@ -6,10 +6,12 @@ const AuthController = {
   login: async (req: Request, res: Response) => {
     try {
       const { email, password } = req.body;
-      if (!email || !password) {
-        res.status(400).json({ message: 'Missing email or password' });
+      if (!email) {
+        res.status(400).json({ message: 'Email is required' });
       }
-
+      if (!password) {
+        res.status(400).json({ message: 'Password is required' });
+      }
       const token = await AuthService.login(req.body);
       res.status(200).json({ token });
     } catch (error: any) {
