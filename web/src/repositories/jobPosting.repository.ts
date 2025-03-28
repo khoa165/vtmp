c76a8977e050e8a50d02f9d2c7b90feb1a0ebf76
Hong An Tran <hongantran3804@gmail.com>
2025-03-28 14:09:46 -0700

    Add jobPosting repo, jobPosting repo test and update jobPosting model

--- web/src/repositories/jobPosting.repository.ts
+++ web/src/repositories/jobPosting.repository.ts
@@ -1,5 +1,36 @@
 import JobPosting from '@/models/jobPosting.model';
+import mongoose from 'mongoose';
 
-const JobPostingRepository = {};
+const { ObjectId } = mongoose.Types;
+
+const JobPostingRepository = {
+  createJobPost: async (jobPostingData: object) => {
+    const newJobPosting = new JobPosting(jobPostingData);
+    await newJobPosting.save();
+    
+    return newJobPosting;
+  },
+  updateById: async (jobId: string, newUpdate: object) => {
+    const updatedJobPosting = await JobPosting.findByIdAndUpdate(
+      new ObjectId(jobId),
+      { $set: newUpdate },
+      { new: true, runValidators: true }
+    );
+
+    return updatedJobPosting;
+  },
+  deleteById: async (jobId: string) => {
+    const currentDate: Date = new Date();
+    const dateDeleted: Date = new Date(currentDate.getDate() + 7);
+
+    const deletedJobPosting = await JobPosting.findByIdAndUpdate(
+      new ObjectId(jobId),
+      { $set: { deletedAt: dateDeleted } },
+      { new: true, runValidators: true }
+    );
+
+    return deletedJobPosting;
+  },
+};
 
 export default JobPostingRepository;
---
+++ web/src/repositories/jobPostingRepository.test.ts
@@ -0,0 +1,71 @@
+import * as chai from 'chai';
+import chaiSubset from 'chai-subset';
+
+import JobPostingRepository from './jobPosting.repository';
+import JobPosting from '@/models/jobPosting.model';
+import { useMongoDB } from '@/config/mongodb.testutils';
+import mongoose from 'mongoose';
+
+chai.use(chaiSubset);
+const { expect } = chai;
+
+describe('JobPostingRepository', () => {
+  useMongoDB();
+
+  const mockJobPosting = {
+    linkId: new mongoose.Types.ObjectId(), // This should be a valid ObjectId from a Link document in your test database
+    url: 'http://example.com/job-posting', // Example URL
+    jobTitle: 'Software Engineer', // Example job title
+    companyName: 'Example Company', // Example company name
+    submittedBy: new mongoose.Types.ObjectId(), // This should be a valid ObjectId from a User document in your test database
+  };
+
+  describe('CreateJobPost for JobPostingRepository', () => {
+    it('should be able to create a new job posting', async () => {
+      const newJobPosting = await JobPostingRepository.createJobPost(
+        mockJobPosting
+      );
+
+      expect(newJobPosting).to.containSubset(mockJobPosting);
+    });
+  });
+
+  describe('UpdateById for JobPostingRepository', () => {
+    it('should be able to update detail of a job post by id', async () => {
+      const newJobPosting = await JobPosting.create(mockJobPosting);
+
+      const newUpdate = {
+        jobTitle: 'Senior Software Engineer', // Updated job title
+        companyName: 'Updated Company', // Updated company name
+        jobDescription: 'This is an updated job description.', // Updated job description
+      };
+
+      const jobId = newJobPosting._id as string;
+      const updatedJobPosting = await JobPostingRepository.updateById(
+        jobId,
+        newUpdate
+      );
+
+      expect(updatedJobPosting).to.containSubset(newUpdate);
+    });
+  });
+
+  describe('DeleteById for JobPostingRepository', () => {
+    it('should be able to set a delete-timestamp for a job posting by id', async () => {
+      const newJobPosting = await JobPosting.create(mockJobPosting);
+
+      const currentDate: Date = new Date();
+      const dateDeleted: Date = new Date(currentDate.getDate() + 7);
+      const updateDelete = {
+        deletedAt: dateDeleted,
+      };
+
+      const jobId = newJobPosting._id as string;
+      const deletedJobPosting = await JobPostingRepository.deleteById(
+        jobId.toString()
+      );
+
+      expect(deletedJobPosting).to.containSubset(updateDelete);
+    });
+  });
+});
