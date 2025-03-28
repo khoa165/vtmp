1868ec886b092560e70766f014cd44c9cf6e3f86
dsmai <son.dmai@gmail.com>
2025-03-28 02:31:46 -0400

    Added integration test for application controller.

---
+++ web/src/controllers/application.controller.test.ts
@@ -0,0 +1,97 @@
+import app from '@/app';
+import request from 'supertest';
+import { expect } from 'chai';
+import { useMongoDB } from '@/config/mongodb.testutils';
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+import JobPosting from '@/models/jobPosting.model';
+import User from '@/models/user.model';
+import UserService from '@/services/user.service';
+
+describe('POST /applications', () => {
+  useMongoDB();
+
+  let mockJobPosting: {
+    linkId: string;
+    url: string;
+    jobTitle: string;
+    companyName: string;
+    submittedBy: string;
+  };
+
+  let newJobPostingId: string;
+
+  let newUserId: string;
+
+  let encryptedPassword: string;
+
+  let mockUser: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    encryptedPassword: string;
+  };
+
+  let token: string;
+
+  beforeEach(async () => {
+    // Create job posting and save to database
+    mockJobPosting = {
+      linkId: new mongoose.Types.ObjectId().toString(),
+      url: 'vtmp.com',
+      jobTitle: 'SWE',
+      companyName: 'Apple',
+      submittedBy: new mongoose.Types.ObjectId().toString(),
+    };
+    newJobPostingId = (await JobPosting.create(mockJobPosting)).id;
+
+    // Create user and save to database
+    encryptedPassword = await bcrypt.hash('test password', 10);
+    mockUser = {
+      firstName: 'admin',
+      lastName: 'viettech',
+      email: 'test@gmail.com',
+      encryptedPassword,
+    };
+    newUserId = (await User.create(mockUser)).id;
+
+    // Mock create token
+    token = await UserService.login({
+      email: mockUser.email,
+      password: 'test password',
+    });
+
+    // Mock middleware dynamically
+    // app.use((req, res, next) => {
+    //   req.user = { id: newUserId, email: 'some-email' };
+    //   console.log('Mock auth middleware runs with userId', newUserId);
+    //   next();
+    // });
+  });
+
+  it.skip('should return error message if request body schema is invalid', (done) => {});
+
+  it.skip('should return error message if user is not authenticated (req.user is null)', (done) => {});
+
+  it('should return application object if an application is created successfully', (done) => {
+    request(app)
+      .post('/api/applications/create')
+      .send({ jobPostingId: newJobPostingId })
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        expect(res.statusCode).to.equal(201);
+        expect(res.body).to.have.property(
+          'message',
+          'Application created successfully'
+        );
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.have.property('jobPostingId', newJobPostingId);
+        expect(res.body.data).to.have.property('userId', newUserId);
+        done();
+      });
+  });
+});
