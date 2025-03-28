1d362dd9da1fac81f160b0d35186dbf543e2b909
Hong An Tran <hongantran3804@gmail.com>
2025-03-28 09:20:47 -0700

    Add controller for jobPosting

--- web/src/controllers/jobPosting.controller.ts
+++ web/src/controllers/jobPosting.controller.ts
@@ -1,6 +1,48 @@
 import { Request, Response } from 'express';
 import JobPostingService from '@/services/jobPosting.service';
-
-const JobPostingController = {};
+import ResouceNotFoundError from '../utils/errors';
+const JobPostingController = {
+  updateJobPosting: async (req: Request, res: Response) => {
+    try {
+      const { id }: { id?: string } = req.params;
+      if (!id) {
+        res.status(400).json({ message: 'ID is required' });
+        return;
+      }
+      const updatedJobPosting = await JobPostingService.updateById(
+        id,
+        req.body
+      );
+      res.status(200).json({
+        data: updatedJobPosting,
+        message: 'Job posting updated successfully',
+      });
+    } catch (error) {
+      if (error instanceof ResouceNotFoundError) {
+        res.status(error.metadata.status).json({ message: error.message });
+      }
+    }
+    res.status(500).json({ message: 'An error occurred' });
+  },
+  deleteJobPosting: async (req: Request, res: Response) => {
+    try {
+      const { id }: { id?: string } = req.params;
+      if (!id) {
+        res.status(400).json({ message: 'ID is required' });
+        return;
+      }
+      const deletedJobPosting = await JobPostingService.deleteById(id);
+      res.status(200).json({
+        data: deletedJobPosting,
+        message: 'Job posting deleted successfully',
+      });
+    } catch (error) {
+      if (error instanceof ResouceNotFoundError) {
+        res.status(error.metadata.status).json({ message: error.message });
+      }
+    }
+    res.status(500).json({ message: 'An error occurred' });
+  },
+};
 
 export default JobPostingController;
