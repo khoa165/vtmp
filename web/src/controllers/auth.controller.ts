b0529bbaabc68dad36d23c3bc32b95368e550280
Nam Nguyen <nguy1053@msu.edu>
2025-03-28 14:24:28 -0400

    Auth Controller: Add explicit return res when we want to respond

--- web/src/controllers/auth.controller.ts
+++ web/src/controllers/auth.controller.ts
@@ -7,16 +7,16 @@ const AuthController = {
     try {
       const { email, password } = req.body;
       if (!email) {
-        res.status(400).json({ message: 'Email is required' });
+        return res.status(400).json({ message: 'Email is required' });
       }
       if (!password) {
-        res.status(400).json({ message: 'Password is required' });
+        return res.status(400).json({ message: 'Password is required' });
       }
       const token = await AuthService.login(req.body);
-      res.status(200).json({ token });
+      return res.status(200).json({ token });
     } catch (error: any) {
       const errorMessage = error?.message ?? 'An error occurred';
-      res.status(401).json({ message: errorMessage });
+      return res.status(401).json({ message: errorMessage });
     }
   },
 };
