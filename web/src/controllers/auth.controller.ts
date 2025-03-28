a080a10b214d2be88c458be9b82af7e257cd3cfd
Nam Nguyen <nguy1053@msu.edu>
2025-03-28 15:37:56 -0400

    Auth Controller: add zod to validate request

--- web/src/controllers/auth.controller.ts
+++ web/src/controllers/auth.controller.ts
@@ -1,19 +1,27 @@
 import AuthService from '@/services/auth.service';
 import { Request, Response } from 'express';
+import { z } from 'zod';
+
+const loginSchema = z.object({
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email({ message: 'Invalid email address' }),
+  password: z.string({ required_error: 'Password is required' }),
+});
 
 const AuthController = {
   /// PARSING, VALIDATING, RESPONSE
   login: async (req: Request, res: Response) => {
     try {
-      const { email, password } = req.body;
-      if (!email) {
-        return res.status(400).json({ message: 'Email is required' });
+      const validatedBody = loginSchema.safeParse(req.body);
+      if (validatedBody.success) {
+        const token = await AuthService.login(validatedBody.data);
+        return res.status(200).json({ token });
+      } else {
+        return res
+          .status(400)
+          .json({ message: validatedBody.error.issues[0]?.message });
       }
-      if (!password) {
-        return res.status(400).json({ message: 'Password is required' });
-      }
-      const token = await AuthService.login(req.body);
-      return res.status(200).json({ token });
     } catch (error: any) {
       const errorMessage = error?.message ?? 'An error occurred';
       return res.status(401).json({ message: errorMessage });
