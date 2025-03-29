9f4456ef3f00b065afe0ddb8a4e10469b25f03e2
Duy Kha <duykha121314888@gmail.com>
2025-03-28 19:24:45 -0500

    Set up test

--- web/src/controllers/auth.controller.ts
+++ web/src/controllers/auth.controller.ts
@@ -1,13 +1,48 @@
 import { Request, Response } from 'express';
-import AuthenticationService from '@/services/auth.service';
+import AuthService from '@/services/auth.service';
+import { Role } from '@/types/interface';
 
 const AuthController = {
   signup: async (req: Request, res: Response) => {
     try {
-      // Call the authentication service - signup
-      const token = await AuthenticationService.signup(req.body);
+      const { firstName, lastName, email, encryptedPassword, role } = req.body;
 
-      //   Return the response
+      // Validate all the fields are required
+      if (!firstName) {
+        return res.status(400).json({ message: 'Firstname is required' });
+      }
+
+      if (!lastName) {
+        return res.status(400).json({ message: 'Lastname is required' });
+      }
+
+      if (!email) {
+        return res.status(400).json({ message: 'Email is required' });
+      }
+
+      if (!encryptedPassword) {
+        return res.status(400).json({ message: 'Password is required' });
+      }
+
+      if (!role) {
+        return res.status(400).json({ message: 'Role is required' });
+      }
+
+      // Check if role's value is the Role enum
+      if (!Object.values(Role).includes(role as Role)) {
+        return res.status(400).json({ message: 'Invalid Role Value' });
+      }
+
+      // Call the authentication service - signup
+      const token = await AuthService.signup({
+        firstName,
+        lastName,
+        email,
+        encryptedPassword,
+        role,
+      });
+
+      // Return the response
       res.status(200).json({ token });
     } catch (error) {
       const errorMessage =
--- web/src/services/auth.service.ts
+++ web/src/services/auth.service.ts
@@ -6,10 +6,18 @@ import jwt from 'jsonwebtoken';
 
 const AuthService = {
   signup: async (accounts: IUser) => {
+    // Check duplicate email
+    const userByEmail = await UserRepository.findByEmail(accounts.email);
+
+    if (userByEmail) {
+      throw new Error('Duplicate Email');
+    }
+
+    // Create new user
     const newUser = await UserRepository.create(accounts);
 
     if (!newUser) {
-      throw new Error('Too bad! You failed to create new user!');
+      throw new Error('Too bad! You failed to sign up!');
     }
 
     return newUser;
