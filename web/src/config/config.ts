9462b34d7bfb234c69d2024ddbba9b99b74240d3
Nam Nguyen <nguy1053@msu.edu>
2025-03-31 10:05:44 -0400

    Use CONFIG in database.ts to check whether CI tests work

--- web/src/config/config.ts
+++ web/src/config/config.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 dotenv.config();
 
 const configSchema = z.object({
-  MONGO_URI: z.string().url(),
+  MONGODB_URI: z.string().url(),
   PORT: z.preprocess(Number, z.number().positive().finite()),
   JWT_SECRET: z.string(),
 });
