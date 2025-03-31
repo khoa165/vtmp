99ebe369f5b5fa01e612848a016c6308ffd04e5e
dsmai <son.dmai@gmail.com>
2025-03-31 17:11:28 -0400

    Export ApplicationStatus enum

--- web/src/models/application.model.ts
+++ web/src/models/application.model.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document } from 'mongoose';
 
-enum ApplicationStatus {
+export enum ApplicationStatus {
   SUBMITTED = 'SUBMITTED',
   IN_PROGRESS = 'IN_PROGRESS',
   OFFER = 'OFFER',
