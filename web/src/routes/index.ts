069be5fd576605610f2a579e3b77227a5adb5af4
Duy Kha <duykha121314888@gmail.com>
2025-03-31 19:14:38 -0500

    fix name

--- web/src/routes/index.ts
+++ web/src/routes/index.ts
@@ -5,7 +5,7 @@ import ApplicationRoutes from '@/routes/application.routes';
 import InterviewRoutes from '@/routes/interview.routes';
 import JobPostingRoutes from '@/routes/jobPosting.routes';
 import LinkRoutes from '@/routes/link.routes';
-import AuthenticationRoutes from './auth.routes';
+import AuthRoutes from './auth.routes';
 
 const router = Router();
 
@@ -15,6 +15,6 @@ router.use('/applications', ApplicationRoutes); // Application module
 router.use('/interviews', InterviewRoutes); // Interview module
 router.use('/job-postings', JobPostingRoutes); // Job Posting module
 router.use('/links', LinkRoutes); // Link module
-router.use('/auth', AuthenticationRoutes); // Authentication module
+router.use('/auth', AuthRoutes); // Authentication module
 
 export default router;
