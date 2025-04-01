6e6e6142b28fa393f17a74909f04b1ff3cba0ff5
Hong An Tran <hongantran3804@gmail.com>
2025-03-31 23:59:54 -0700

    Update jobPosting.service.ts

--- web/src/services/jobPostingService.test.ts
+++ web/src/services/jobPostingService.test.ts
@@ -1,9 +1,13 @@
 import * as chai from 'chai';
+import assert from 'assert';
 
 import JobPostingService from './jobPosting.service';
 import JobPostingRepository from '@/repositories/jobPosting.repository';
 import { useMongoDB } from '@/testutils/mongoDB.testutil';
 import mongoose from 'mongoose';
+import { ResourceNotFoundError } from '@/utils/errors';
+import { differenceInSeconds } from 'date-fns';
+
 const { expect } = chai;
 
 describe('JobPostingService', () => {
@@ -17,21 +21,20 @@ describe('JobPostingService', () => {
     submittedBy: new mongoose.Types.ObjectId(),
   };
 
-  describe('updateJobPostingById', () => {
+  describe.only('updateJobPostingById', () => {
     it('should throw an error when no job posting is found', async () => {
-      await JobPostingRepository.createJobPosting(mockJobPosting);
       const newUpdate = {
         jobTitle: 'Senior Software Engineer',
         companyName: 'Updated Company',
         jobDescription: 'This is an updated job description.',
       };
 
-      expect(
+      await expect(
         JobPostingService.updateJobPostingById(
           new mongoose.Types.ObjectId().toString(),
           newUpdate
         )
-      ).to.throw;
+      ).eventually.rejectedWith(ResourceNotFoundError);
     });
 
     it('should not throw an error when a job posting is found', async () => {
@@ -46,19 +49,36 @@ describe('JobPostingService', () => {
 
       expect(
         JobPostingService.updateJobPostingById(newJobPosting.id, newUpdate)
-      ).to.not.throw;
+      ).eventually.fulfilled;
+    });
+
+    it('should be able to update detail of a job post by id', async () => {
+      const newJobPosting = await JobPostingRepository.createJobPosting(
+        mockJobPosting
+      );
+
+      const newUpdate = {
+        jobTitle: 'Senior Software Engineer',
+        companyName: 'Updated Company',
+        jobDescription: 'This is an updated job description.',
+      };
+
+      const updatedJobPosting = await JobPostingRepository.updateJobPostingById(
+        newJobPosting.id,
+        newUpdate
+      );
+
+      expect(updatedJobPosting).to.containSubset(newUpdate);
     });
   });
 
-  describe('deleteJobPostingById', () => {
+  describe.only('deleteJobPostingById', () => {
     it('should throw an error when no job posting is found', async () => {
-      await JobPostingRepository.createJobPosting(mockJobPosting);
-
       expect(
         JobPostingService.deleteJobPostingById(
           new mongoose.Types.ObjectId().toString()
         )
-      ).to.throw;
+      ).eventually.rejectedWith(ResourceNotFoundError);
     });
 
     it('should not throw an error when a job posting is found', async () => {
@@ -66,8 +86,33 @@ describe('JobPostingService', () => {
         mockJobPosting
       );
 
-      expect(JobPostingService.deleteJobPostingById(newJobPosting.id)).to.not
-        .throw;
+      expect(JobPostingService.deleteJobPostingById(newJobPosting.id)).eventually.fulfilled;
+    });
+
+    it('should be able to set a delete-timestamp for a job posting by id', async () => {
+      const newJobPosting = await JobPostingRepository.createJobPosting(
+        mockJobPosting
+      );
+      const deletedJobPosting = await JobPostingRepository.deleteJobPostingById(
+        newJobPosting.id
+      );
+
+      assert(
+        deletedJobPosting !== null,
+        'Deleted job posting must not be null'
+      );
+      assert(
+        'deletedAt' in deletedJobPosting,
+        'Deleted job posting must have a deletedAt property'
+      );
+
+      const timeDiff = differenceInSeconds(
+        new Date(),
+        deletedJobPosting.deletedAt
+      );
+
+      expect(deletedJobPosting).to.not.be.null;
+      expect(timeDiff).to.lessThan(3);
     });
   });
 });
