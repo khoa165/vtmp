99a87ddc18150f17f07f5aee20ec8d90c7792c49
Duy Kha <duykha121314888@gmail.com>
2025-04-03 21:30:31 -0500

    create constants folder and update object name

---
+++ web/src/constants/constants.ts
@@ -0,0 +1,23 @@
+import { Role, Permission } from '@/types/enums';
+
+// Mapping Role with Permission
+export const roleToPermissionMapping = {
+  [Role.ADMIN]: [
+    Permission.GET_ALL_PENDING_JOB_LINKS,
+    Permission.APPROVE_JOB_LINK,
+    Permission.REJECT_JOB_LINK,
+    Permission.UPDATE_JOB_POSTING,
+    Permission.DELETE_JOB_POSTING,
+    Permission.GET_ALL_INVITATIONS,
+    Permission.CREATE_INVITATION,
+    Permission.REVOKE_INVITATION,
+  ],
+  [Role.MODERATOR]: [
+    Permission.GET_ALL_PENDING_JOB_LINKS,
+    Permission.APPROVE_JOB_LINK,
+    Permission.REJECT_JOB_LINK,
+    Permission.UPDATE_JOB_POSTING,
+    Permission.DELETE_JOB_POSTING,
+  ],
+  [Role.USER]: [],
+};
