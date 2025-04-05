41013dc063e24db1886b279ad1aac419d97cebe2
Khoa Le <khoatle165@gmail.com>
2025-04-04 23:03:13 -0400

    In-progress frontend setup

---
+++ web-client/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*'],
+      exclude: [],
+    },
+  },
+  assetsInclude: ['src/**/*.md'],
+});
