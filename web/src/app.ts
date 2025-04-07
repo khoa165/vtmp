792e09379d7c3d270a37cc3e766049d90dcd8e6c
Nam Nguyen <nguy1053@msu.edu>
2025-04-07 11:53:53 -0400

    Create global route error handler to remove try-catch in controller.

--- web/src/app.ts
+++ web/src/app.ts
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import connectDB from '@/config/database';
 import routes from '@/routes/index';
+import { routeErrorHandler } from '@/middlewares/utils';
 
 dotenv.config();
 
@@ -31,6 +32,7 @@ app.get('/health', (_req: Request, res: Response) => {
 
 // Routes
 app.use('/api', routes);
+app.use(routeErrorHandler);
 app.use('*', (_req: Request, res: Response) => {
   res.status(404).send('Not found');
 });
