d86463608c6bfb415d0c43d306fc80c06ba2fb4a
Duy Kha <duykha121314888@gmail.com>
2025-04-08 01:28:48 -0500

    update test suits

--- web/src/middlewares/auth.middleware.test.ts
+++ web/src/middlewares/auth.middleware.test.ts
@@ -2,9 +2,7 @@ import * as chai from 'chai';
 import chaiSubset from 'chai-subset';
 import app from '@/app';
 import request from 'supertest';
-import { UserRepository } from '@/repositories/user.repository';
 import { useMongoDB } from '@/testutils/mongoDB.testutil';
-import bcrypt from 'bcryptjs';
 import { EnvConfig } from '@/config/env';
 import jwt from 'jsonwebtoken';
 import { expectErrorsArray } from '@/testutils/response-assertion.testutil';
@@ -14,21 +12,6 @@ const { expect } = chai;
 describe('UserRepository', () => {
   useMongoDB();
 
-  beforeEach(async () => {
-    const saltRounds = 10;
-    const password = 'ecnrypted-password-later';
-    const encryptedPassword = await bcrypt.hash(password, saltRounds);
-
-    const mockUser = {
-      firstName: 'admin',
-      lastName: 'viettech',
-      email: 'test@example.com',
-      encryptedPassword,
-    };
-
-    await UserRepository.createUser(mockUser);
-  });
-
   describe.only('AuthMiddleware', () => {
     it('should throw Unauthorized for no token', async () => {
       const res = await request(app)
@@ -36,7 +19,6 @@ describe('UserRepository', () => {
         .send({})
         .set('Accept', 'application/json')
         .set('Authorization', ``);
-      console.log(res.body);
       expectErrorsArray({ res, statusCode: 401, errorsCount: 1 });
     });
 
@@ -46,16 +28,25 @@ describe('UserRepository', () => {
         .send({})
         .set('Accept', 'application/json')
         .set('Authorization', `fake token`);
-      console.log(res.body);
       expectErrorsArray({ res, statusCode: 403, errorsCount: 1 });
     });
 
     it('should allow user to login, return a token and get user profile', async () => {
+      await request(app)
+        .post('/api/auth/signup')
+        .send({
+          firstName: 'admin',
+          lastName: 'viettech',
+          email: 'test@example.com',
+          password: 'Test!123',
+        })
+        .set('Accept', 'application/json');
+
       const resLogin = await request(app)
         .post('/api/auth/login')
         .send({
           email: 'test@example.com',
-          password: 'ecnrypted-password-later',
+          password: 'Test!123',
         })
         .set('Accept', 'application/json');
 
