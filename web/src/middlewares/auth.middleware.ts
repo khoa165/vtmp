c90777fbc10581dcd3a9ceba8b5b96f87abe19cd
Duy Kha <duykha121314888@gmail.com>
2025-04-08 00:17:59 -0500

    Auth code

--- web/src/middlewares/auth.middleware.ts
+++ web/src/middlewares/auth.middleware.ts
@@ -1,10 +1,11 @@
+import { ForbiddenError, UnauthorizedError } from '@/utils/errors';
 import { EnvConfig } from '@/config/env';
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { z } from 'zod';
 
 const DecodedJWTSchema = z.object({
-  id: z.string(),
+  id: z.string({ required_error: 'Id is required' }),
 });
 
 export const authenticate = (
@@ -12,21 +13,39 @@ export const authenticate = (
   res: Response,
   next: NextFunction
 ): void => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization;
+
   if (!token) {
-    res.status(401).json({ message: 'Unauthorized' });
+    const error = new UnauthorizedError('Unauthorized', {});
+    const { statusCode, message } = error;
+
+    res.status(statusCode).json({ message });
     return;
   }
 
   try {
-    const decoded = jwt.verify(token, EnvConfig.get().JWT_SECRET);
+    const decoded = jwt.verify(
+      token,
+      EnvConfig.get().JWT_SECRET,
+      (err, decoded) => {
+        if (err) {
+          console.log('Token verification failed:', err);
+        } else {
+          console.log('Decoded token:', decoded);
+        }
+      }
+    );
     const parsed = DecodedJWTSchema.safeParse(decoded);
+
     if (parsed.success) {
       req.user = parsed.data;
     }
 
     next();
   } catch {
-    res.status(403).json({ message: 'Forbidden' });
+    const error = new ForbiddenError('Forbidden', {});
+    const { statusCode, message } = error;
+
+    res.status(statusCode).json({ message });
   }
 };
