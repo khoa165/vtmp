285a9cdb94fc0bcfe46cf3d0662562b0e0469625
QuangMinhNguyen27405 <quangminh27405@gmail.com>
2025-04-09 12:58:32 -0400

    Completed repository layer for interview

--- web/src/repositories/interview.repository.ts
+++ web/src/repositories/interview.repository.ts
@@ -4,11 +4,19 @@ export const InterviewRepository = {
   createInterview: async (applicationId: string) => {
     return InterviewModel.create(applicationId);
   },
-  getInterviewByApplicatonId: async (applicationId: string) => {
+  getInterview: async (interviewId: string) => {
+    return InterviewModel.findById({ interviewId });
+  },
+  getInterviewsByUserId: async (userId: string) => {
+    return InterviewModel.find({ userId });
+  },
+  getInterviewsByApplicatonId: async (applicationId: string) => {
     return InterviewModel.find({ applicationId });
   },
-  getInterviewByStatus: async ({ status }: { status: string }) => {
-    return InterviewModel.find({ status: status });
+  updateInterview: async (interviewId: string) => {
+    return InterviewModel.findByIdAndUpdate(interviewId);
   },
-  updateInterviewWithStatus: async ({}) => {},
+  deleteInterview: async (interviewId: string) => {
+    return InterviewModel.findByIdAndDelete(interviewId);
+  },
 };
