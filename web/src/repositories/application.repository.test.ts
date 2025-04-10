193b0b043b975edb412df95d7fe892710316d397
dsmai <son.dmai@gmail.com>
2025-04-10 03:02:17 -0400

    Added all repository functions for application. Added corresponding unit tests. Added deletedAt field to query list for getApplications, getApplicationId, and added test cases to test against soft-deleting

--- web/src/repositories/application.repository.test.ts
+++ web/src/repositories/application.repository.test.ts
@@ -4,7 +4,7 @@ import { differenceInSeconds } from 'date-fns';
 
 import { ApplicationRepository } from './application.repository';
 import { useMongoDB } from '@/testutils/mongoDB.testutil';
-import { ApplicationStatus } from '@/types/enums';
+import { ApplicationStatus, InterestLevel } from '@/types/enums';
 import { getNewMongoId, toMongoId } from '@/testutils/mongoID.testutil';
 
 describe('ApplicationRepository', () => {
@@ -85,6 +85,26 @@ describe('ApplicationRepository', () => {
       });
     });
 
+    it('should exclude soft-deleted application from list of returned application', async () => {
+      await ApplicationRepository.createApplication(mockApplication_A0);
+      const mockApplicationId_A1 = (
+        await ApplicationRepository.createApplication(mockApplication_A1)
+      ).id;
+      await ApplicationRepository.deleteApplicationById(
+        userId_A,
+        mockApplicationId_A1
+      );
+      const applications =
+        await ApplicationRepository.getApplications(userId_A);
+
+      assert(applications);
+      expect(applications).to.be.an('array').that.have.lengthOf(1);
+      expect(applications[0]).to.containSubset({
+        jobPostingId: toMongoId(mockApplication_A0.jobPostingId),
+        userId: toMongoId(mockApplication_A0.userId),
+      });
+    });
+
     it('should return an empty array if no applications found for userId', async () => {
       await ApplicationRepository.createApplication(mockApplication_B);
       const applications =
@@ -96,7 +116,7 @@ describe('ApplicationRepository', () => {
   });
 
   describe('getApplicationById', () => {
-    it('should return no application if the application is not belong to the authenticated user', async () => {
+    it('should return null if the application is not belong to the authenticated user', async () => {
       const mockApplicationId_B = (
         await ApplicationRepository.createApplication(mockApplication_B)
       ).id;
@@ -106,6 +126,22 @@ describe('ApplicationRepository', () => {
       });
 
       assert(!application);
+    });
+
+    it('should return null if trying to get soft-deleted application', async () => {
+      const mockApplicationId_B = (
+        await ApplicationRepository.createApplication(mockApplication_B)
+      ).id;
+      await ApplicationRepository.deleteApplicationById(
+        userId_B,
+        mockApplicationId_B
+      );
+      const foundApplication = await ApplicationRepository.getApplicationById({
+        applicationId: mockApplicationId_B,
+        userId: userId_B,
+      });
+
+      assert(!foundApplication);
     });
 
     it('should return an application if there exists an application for authenticated user', async () => {
@@ -124,6 +160,160 @@ describe('ApplicationRepository', () => {
         jobPostingId: toMongoId(mockApplication_A1.jobPostingId),
         userId: toMongoId(mockApplication_A1.userId),
       });
+    });
+  });
+
+  describe('updateApplicationStatus', () => {
+    it('should return null if application does not exist', async () => {
+      const updatedApplication =
+        await ApplicationRepository.updateApplicationStatus(
+          userId_A,
+          getNewMongoId(),
+          ApplicationStatus.OFFER
+        );
+
+      assert(!updatedApplication);
+    });
+
+    it('should return null if trying to update status of a soft-deleted application', async () => {
+      const mockApplicationId_B = (
+        await ApplicationRepository.createApplication(mockApplication_B)
+      ).id;
+      await ApplicationRepository.deleteApplicationById(
+        userId_B,
+        mockApplicationId_B
+      );
+      const updatedApplication =
+        await ApplicationRepository.updateApplicationStatus(
+          userId_B,
+          mockApplicationId_B,
+          ApplicationStatus.OFFER
+        );
+
+      assert(!updatedApplication);
+    });
+
+    it('should return updated application with new status if application found', async () => {
+      const mockApplicationId_B = (
+        await ApplicationRepository.createApplication(mockApplication_B)
+      ).id;
+      const updatedApplication =
+        await ApplicationRepository.updateApplicationStatus(
+          userId_B,
+          mockApplicationId_B,
+          ApplicationStatus.OFFER
+        );
+
+      assert(updatedApplication);
+      expect(updatedApplication.status).to.be.equal(ApplicationStatus.OFFER);
+    });
+  });
+
+  describe('updateApplicationMetadata', () => {
+    it('should return null if application does not exist', async () => {
+      const updatedApplication =
+        await ApplicationRepository.updateApplicationMetadata(
+          userId_A,
+          getNewMongoId(),
+          {}
+        );
+
+      assert(!updatedApplication);
+    });
+
+    it('should return null if trying to update metadata of a soft-deleted application', async () => {
+      const mockApplicationId_B = (
+        await ApplicationRepository.createApplication(mockApplication_B)
+      ).id;
+      await ApplicationRepository.deleteApplicationById(
+        userId_B,
+        mockApplicationId_B
+      );
+      const updatedApplication =
+        await ApplicationRepository.updateApplicationMetadata(
+          userId_B,
+          mockApplicationId_B,
+          {}
+        );
+
+      assert(!updatedApplication);
+    });
+
+    it('should return updated application with new metadata if application found', async () => {
+      const mockApplicationId_B = (
+        await ApplicationRepository.createApplication(mockApplication_B)
+      ).id;
+      const updatedApplication =
+        await ApplicationRepository.updateApplicationMetadata(
+          userId_B,
+          mockApplicationId_B,
+          {
+            note: 'note about this application',
+            referrer: 'Khoa',
+            interest: InterestLevel.HIGH,
+          }
+        );
+
+      assert(updatedApplication);
+      expect(updatedApplication.referrer).to.be.equal('Khoa');
+      expect(updatedApplication.interest).to.be.equal(InterestLevel.HIGH);
+      expect(updatedApplication.note).to.be.equal(
+        'note about this application'
+      );
+    });
+  });
+
+  describe('deleteApplicationById', () => {
+    it('should return null if application does not exist', async () => {
+      const deletedApplication =
+        await ApplicationRepository.deleteApplicationById(
+          userId_A,
+          getNewMongoId()
+        );
+
+      assert(!deletedApplication);
+    });
+
+    it('should return null if trying to delete an already soft-deleted application', async () => {
+      const mockApplicationId_B = (
+        await ApplicationRepository.createApplication(mockApplication_B)
+      ).id;
+      await ApplicationRepository.deleteApplicationById(
+        userId_B,
+        mockApplicationId_B
+      );
+      const deletedApplication =
+        await ApplicationRepository.deleteApplicationById(
+          userId_B,
+          mockApplicationId_B
+        );
+
+      assert(!deletedApplication);
+    });
+
+    it('should soft delete application and return deleted application object with deletedAt field set', async () => {
+      const mockApplicationId_B = (
+        await ApplicationRepository.createApplication(mockApplication_B)
+      ).id;
+      const deletedApplication =
+        await ApplicationRepository.deleteApplicationById(
+          userId_B,
+          mockApplicationId_B
+        );
+
+      assert(deletedApplication);
+      assert(deletedApplication.deletedAt);
+      const timeDiff = differenceInSeconds(
+        new Date(),
+        deletedApplication.deletedAt
+      );
+      expect(timeDiff).to.lessThan(3);
+
+      const foundApplication = await ApplicationRepository.getApplicationById({
+        applicationId: mockApplicationId_B,
+        userId: userId_B,
+      });
+      assert(!foundApplication);
     });
   });
 });
--- web/src/repositories/application.repository.ts
+++ web/src/repositories/application.repository.ts
@@ -1,4 +1,5 @@
-import { ApplicationModel } from '@/models/application.model';
+import { ApplicationModel, IApplication } from '@/models/application.model';
+import { ApplicationStatus, InterestLevel } from '@/types/enums';
 
 export const ApplicationRepository = {
   createApplication: async ({
@@ -7,7 +8,7 @@ export const ApplicationRepository = {
   }: {
     jobPostingId: string;
     userId: string;
-  }) => {
+  }): Promise<IApplication> => {
     return ApplicationModel.create({
       jobPostingId,
       userId,
@@ -20,16 +21,17 @@ export const ApplicationRepository = {
   }: {
     jobPostingId: string;
     userId: string;
-  }) => {
+  }): Promise<boolean> => {
     return !!(await ApplicationModel.exists({
       jobPostingId,
       userId,
     }));
   },
 
-  getApplications: async (userId: string) => {
+  getApplications: async (userId: string): Promise<IApplication[]> => {
     return ApplicationModel.find({
       userId: userId,
+      deletedAt: null,
     });
   },
 
@@ -39,10 +41,51 @@ export const ApplicationRepository = {
   }: {
     applicationId: string;
     userId: string;
-  }) => {
+  }): Promise<IApplication | null> => {
     return ApplicationModel.findOne({
       _id: applicationId,
       userId: userId,
+      deletedAt: null,
     });
+  },
+
+  updateApplicationStatus: async (
+    userId: string,
+    applicationId: string,
+    updatedStatus: ApplicationStatus
+  ): Promise<IApplication | null> => {
+    return ApplicationModel.findOneAndUpdate(
+      { _id: applicationId, userId, deletedAt: null },
+      { $set: { status: updatedStatus } },
+      { new: true }
+    );
+  },
+
+  updateApplicationMetadata: async (
+    userId: string,
+    applicationId: string,
+    updatedMetadata: {
+      note?: string;
+      referrer?: string;
+      portalLink?: string;
+      interest?: InterestLevel;
+    }
+  ): Promise<IApplication | null> => {
+    return ApplicationModel.findOneAndUpdate(
+      { _id: applicationId, userId, deletedAt: null },
+      updatedMetadata,
+      { new: true }
+    );
+  },
+
+  deleteApplicationById: async (
+    userId: string,
+    applicationId: string
+  ): Promise<IApplication | null> => {
+    return ApplicationModel.findOneAndUpdate(
+      { _id: applicationId, userId, deletedAt: null },
+      { $set: { deletedAt: new Date() } },
+      { new: true }
+    );
   },
 };
