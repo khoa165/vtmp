16f43b2027082bdc7b83972fa525391caf2c44f1
Nam Nguyen <nguy1053@msu.edu>
2025-04-13 17:34:45 -0400

    Use getUserFromRequest utility in user controller to get user id from request object

--- web/src/controllers/user.controller.ts
+++ web/src/controllers/user.controller.ts
@@ -1,8 +1,9 @@
 import UserService from '@/services/user.service';
 import { UserRole } from '@common/enums';
-import { ForbiddenError, handleError } from '@/utils/errors';
+import { ForbiddenError } from '@/utils/errors';
 import { Request, Response } from 'express';
 import { z } from 'zod';
+import { getUserFromRequest } from '@/middlewares/utils';
 
 const UserUpdateSchema = z
   .object({
@@ -28,76 +29,45 @@ const UserIdSchema = z.object({
   id: z.string(),
 });
 
-// TODO: dson - need to figure out how to remove "as AuthenticatedRequest"
-interface AuthenticatedRequest extends Request {
-  user: {
-    id: string;
-  };
-}
-
 const UserController = {
   getAllUsers: async (_req: Request, res: Response) => {
-    try {
-      const users = await UserService.getAllUsers();
-      res.status(200).json({ data: users });
-      return;
-    } catch (err: unknown) {
-      const { statusCode, errors } = handleError(err);
-      res.status(statusCode).json({ errors });
-      return;
-    }
+    const users = await UserService.getAllUsers();
+    res.status(200).json({ data: users });
+    return;
   },
 
   getUser: async (req: Request, res: Response) => {
-    try {
-      const { id } = UserIdSchema.parse(req.params);
-      const userIdFromReq = (req as AuthenticatedRequest).user.id;
-      if (id !== userIdFromReq) {
-        throw new ForbiddenError('Forbidden', { id, userIdFromReq });
-      }
-
-      const user = await UserService.getUserById(id);
-      res.status(200).json({ data: user });
-      return;
-    } catch (err: unknown) {
-      const { statusCode, errors } = handleError(err);
-      res.status(statusCode).json({ errors });
-      return;
+    const { id } = UserIdSchema.parse(req.params);
+    const userIdFromReq = getUserFromRequest(req).user.id;
+    if (id !== userIdFromReq) {
+      throw new ForbiddenError('Forbidden', { id, userIdFromReq });
     }
+
+    const user = await UserService.getUserById(id);
+    res.status(200).json({ data: user });
+    return;
   },
 
   updateUser: async (req: Request, res: Response) => {
-    try {
-      const { id } = UserIdSchema.parse(req.params);
-      const userIdFromReq = (req as AuthenticatedRequest).user.id;
-      if (id !== userIdFromReq) {
-        throw new ForbiddenError('Forbidden', { id, userIdFromReq });
-      }
-
-      const updateUserData = UserUpdateSchema.parse(req.body);
-      const updatedUser = await UserService.updateUserById(id, updateUserData);
-      res.status(200).json({ data: updatedUser });
-      return;
-    } catch (err: unknown) {
-      const { statusCode, errors } = handleError(err);
-      res.status(statusCode).json({ errors });
-      return;
+    const { id } = UserIdSchema.parse(req.params);
+    const userIdFromReq = getUserFromRequest(req).user.id;
+    if (id !== userIdFromReq) {
+      throw new ForbiddenError('Forbidden', { id, userIdFromReq });
     }
+
+    const updateUserData = UserUpdateSchema.parse(req.body);
+    const updatedUser = await UserService.updateUserById(id, updateUserData);
+    res.status(200).json({ data: updatedUser });
+    return;
   },
 
   updateUserRole: async (req: Request, res: Response) => {
-    try {
-      const { id } = UserIdSchema.parse(req.params);
-      const { role } = UserUpdateRoleSchema.parse(req.body);
+    const { id } = UserIdSchema.parse(req.params);
+    const { role } = UserUpdateRoleSchema.parse(req.body);
 
-      const updatedUser = await UserService.updateUserById(id, { role });
-      res.status(200).json({ data: updatedUser });
-      return;
-    } catch (err: unknown) {
-      const { statusCode, errors } = handleError(err);
-      res.status(statusCode).json({ errors });
-      return;
-    }
+    const updatedUser = await UserService.updateUserById(id, { role });
+    res.status(200).json({ data: updatedUser });
+    return;
   },
 };
 
