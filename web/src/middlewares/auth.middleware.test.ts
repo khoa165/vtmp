265ce01bc032a8daefe9212baf40f2cbe938630c
Duy Kha <duykha121314888@gmail.com>
2025-04-27 17:57:02 -0500

    Fix auth test

--- web/src/middlewares/auth.middleware.test.ts
+++ web/src/middlewares/auth.middleware.test.ts
@@ -1,5 +1,4 @@
 import * as chai from 'chai';
-import chaiSubset from 'chai-subset';
 import app from '@/app';
 import request from 'supertest';
 import { useMongoDB } from '@/testutils/mongoDB.testutil';
@@ -11,87 +10,86 @@ import { useSandbox } from '@/testutils/sandbox.testutil';
 import { MOCK_ENV } from '@/testutils/mock-data.testutil';
 import { getNewMongoId } from '@/testutils/mongoID.testutil';
 
-chai.use(chaiSubset);
 const { expect } = chai;
-describe('UserRepository', () => {
+describe(`UserRepository`, () => {
   useMongoDB();
   const sandbox = useSandbox();
   beforeEach(() => {
-    sandbox.stub(EnvConfig, 'get').returns(MOCK_ENV);
+    sandbox.stub(EnvConfig, `get`).returns(MOCK_ENV);
   });
 
-  describe('AuthMiddleware', () => {
-    it('should throw Unauthorized for no token', async () => {
+  describe(`AuthMiddleware`, () => {
+    it(`should throw Unauthorized for no token`, async () => {
       const res = await request(app)
-        .get('/api/users/profile')
+        .get(`/api/users/profile`)
         .send({})
-        .set('Accept', 'application/json')
-        .set('Authorization', ``);
+        .set(`Accept`, `application/json`)
+        .set(`Authorization`, ``);
       expectErrorsArray({ res, statusCode: 401, errorsCount: 1 });
     });
 
-    it('should throw Unauthorized for wrong token', async () => {
+    it(`should throw Unauthorized for wrong token`, async () => {
       const res = await request(app)
-        .get('/api/users/profile')
+        .get(`/api/users/profile`)
         .send({})
-        .set('Accept', 'application/json')
-        .set('Authorization', `fake token`);
+        .set(`Accept`, `application/json`)
+        .set(`Authorization`, `fake token`);
       expectErrorsArray({ res, statusCode: 401, errorsCount: 1 });
     });
 
-    it('should throw ResourceNotFoundError for cannot find user', async () => {
+    it(`should throw ResourceNotFoundError for cannot find user`, async () => {
       const token = jwt.sign(
         { id: getNewMongoId() },
         EnvConfig.get().JWT_SECRET,
         {
-          expiresIn: '1h',
+          expiresIn: `1h`,
         }
       );
 
-      const resGetProfile = await request(app)
-        .get('/api/users/profile')
+      const res = await request(app)
+        .get(`/api/users/profile`)
         .send({})
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token}`);
+        .set(`Accept`, `application/json`)
+        .set(`Authorization`, `Bearer ${token}`);
 
       expectErrorsArray({
-        res: resGetProfile,
+        res,
         statusCode: 404,
         errorsCount: 1,
       });
     });
 
-    it('should allow user to login, return a token and get user profile', async () => {
+    it(`should allow user to login, return a token and get user profile`, async () => {
       await request(app)
-        .post('/api/auth/signup')
+        .post(`/api/auth/signup`)
         .send({
-          firstName: 'admin',
-          lastName: 'viettech',
-          email: 'test@example.com',
-          password: 'Test!123',
+          firstName: `admin`,
+          lastName: `viettech`,
+          email: `test@example.com`,
+          password: `Test!123`,
         })
-        .set('Accept', 'application/json');
+        .set(`Accept`, `application/json`);
 
       const resLogin = await request(app)
-        .post('/api/auth/login')
+        .post(`/api/auth/login`)
         .send({
-          email: 'test@example.com',
-          password: 'Test!123',
+          email: `test@example.com`,
+          password: `Test!123`,
         })
-        .set('Accept', 'application/json');
+        .set(`Accept`, `application/json`);
 
       const token = resLogin.body.data.token;
       const decoded = jwt.verify(token, EnvConfig.get().JWT_SECRET);
       const id = DecodedJWTSchema.parse(decoded).id;
 
-      const resGetProfile = await request(app)
-        .get('/api/users/profile')
+      const res = await request(app)
+        .get(`/api/users/${id}`)
         .send({})
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token}`);
+        .set(`Accept`, `application/json`)
+        .set(`Authorization`, `Bearer ${token}`);
 
-      expect(resGetProfile.body).to.have.property('id');
-      expect(resGetProfile.body.id).to.equal(id);
+      expect(res.body.data).to.have.property(`_id`);
+      expect(res.body.data._id).to.equal(id);
     });
   });
 });
