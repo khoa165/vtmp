df6981627f0c5d6d3747a50fee2cde9cb83878d3
Duy Kha <duykha121314888@gmail.com>
2025-04-27 18:27:23 -0500

    Fix `

--- web/src/middlewares/auth.middleware.test.ts
+++ web/src/middlewares/auth.middleware.test.ts
@@ -11,46 +11,46 @@ import { MOCK_ENV } from '@/testutils/mock-data.testutil';
 import { getNewMongoId } from '@/testutils/mongoID.testutil';
 
 const { expect } = chai;
-describe(`UserRepository`, () => {
+describe('UserRepository', () => {
   useMongoDB();
   const sandbox = useSandbox();
   beforeEach(() => {
-    sandbox.stub(EnvConfig, `get`).returns(MOCK_ENV);
+    sandbox.stub(EnvConfig, 'get').returns(MOCK_ENV);
   });
 
-  describe(`AuthMiddleware`, () => {
-    it(`should throw Unauthorized for no token`, async () => {
+  describe('AuthMiddleware', () => {
+    it('should throw Unauthorized for no token', async () => {
       const res = await request(app)
-        .get(`/api/users/profile`)
+        .get('/api/users/profile')
         .send({})
-        .set(`Accept`, `application/json`)
-        .set(`Authorization`, ``);
+        .set('Accept', 'application/json')
+        .set('Authorization', '');
       expectErrorsArray({ res, statusCode: 401, errorsCount: 1 });
     });
 
-    it(`should throw Unauthorized for wrong token`, async () => {
+    it('should throw Unauthorized for wrong token', async () => {
       const res = await request(app)
-        .get(`/api/users/profile`)
+        .get('/api/users/profile')
         .send({})
-        .set(`Accept`, `application/json`)
-        .set(`Authorization`, `fake token`);
+        .set('Accept', 'application/json')
+        .set('Authorization', 'fake token');
       expectErrorsArray({ res, statusCode: 401, errorsCount: 1 });
     });
 
-    it(`should throw ResourceNotFoundError for cannot find user`, async () => {
+    it('should throw ResourceNotFoundError for cannot find user', async () => {
       const token = jwt.sign(
         { id: getNewMongoId() },
         EnvConfig.get().JWT_SECRET,
         {
-          expiresIn: `1h`,
+          expiresIn: '1h',
         }
       );
 
       const res = await request(app)
-        .get(`/api/users/profile`)
+        .get('/api/users/profile')
         .send({})
-        .set(`Accept`, `application/json`)
-        .set(`Authorization`, `Bearer ${token}`);
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${token}`);
 
       expectErrorsArray({
         res,
@@ -59,24 +59,24 @@ describe(`UserRepository`, () => {
       });
     });
 
-    it(`should allow user to login, return a token and get user profile`, async () => {
+    it('should allow user to login, return a token and get user profile', async () => {
       await request(app)
-        .post(`/api/auth/signup`)
+        .post('/api/auth/signup')
         .send({
-          firstName: `admin`,
-          lastName: `viettech`,
-          email: `test@example.com`,
-          password: `Test!123`,
+          firstName: 'admin',
+          lastName: 'viettech',
+          email: 'test@example.com',
+          password: 'Test!123',
         })
-        .set(`Accept`, `application/json`);
+        .set('Accept', 'application/json');
 
       const resLogin = await request(app)
-        .post(`/api/auth/login`)
+        .post('/api/auth/login')
         .send({
-          email: `test@example.com`,
-          password: `Test!123`,
+          email: 'test@example.com',
+          password: 'Test!123',
         })
-        .set(`Accept`, `application/json`);
+        .set('Accept', 'application/json');
 
       const token = resLogin.body.data.token;
       const decoded = jwt.verify(token, EnvConfig.get().JWT_SECRET);
@@ -85,10 +85,10 @@ describe(`UserRepository`, () => {
       const res = await request(app)
         .get(`/api/users/${id}`)
         .send({})
-        .set(`Accept`, `application/json`)
-        .set(`Authorization`, `Bearer ${token}`);
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${token}`);
 
-      expect(res.body.data).to.have.property(`_id`);
+      expect(res.body.data).to.have.property('_id');
       expect(res.body.data._id).to.equal(id);
     });
   });
