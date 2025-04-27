a6d1dbfe1e3f17b8459ced121109409514872e32
Khoa Le <46258781+khoa165@users.noreply.github.com>
2025-04-27 04:50:47 -0400

    Frontend application tracker scaffold (#121)

--- web-client/src/components/layout/vtmp-wrapper.tsx
+++ web-client/src/components/layout/vtmp-wrapper.tsx
@@ -1,6 +1,7 @@
-import { Outlet } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Outlet } from 'react-router-dom';
+import 'src/styles/scss/app.scss';
 
 export const VTMPWrapper = () => {
   const darkTheme = createTheme({
@@ -8,7 +9,6 @@ export const VTMPWrapper = () => {
       mode: 'dark',
     },
   });
-
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
