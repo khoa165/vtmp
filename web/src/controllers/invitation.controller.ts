bd36596b3706f96cad147a5683ac460e98099500
Duy Kha <duykha121314888@gmail.com>
2025-05-01 14:29:52 -0500

    Invitation Controller Implementation

--- web/src/controllers/invitation.controller.ts
+++ web/src/controllers/invitation.controller.ts
@@ -1,1 +1,46 @@
-export const InvitationController = {};
+import { Request, Response } from 'express';
+import { InvitationService } from '@/services/invitation.service';
+import { z } from 'zod';
+
+const InvitationSendEmailSchema = z.object({
+  receiverName: z.string(),
+  receiverEmail: z.string().email(),
+  senderId: z.string(),
+});
+
+const InvitationIdSchema = z.object({
+  invitationId: z.string(),
+});
+
+const InvitationTokenSchema = z.object({
+  token: z.string(),
+});
+
+export const InvitationController = {
+  getAllInvitations: async (_req: Request, res: Response) => {
+    const invitations = await InvitationService.getAllInvitations();
+    res.status(200).json({ data: invitations });
+  },
+
+  sendInvitation: async (req: Request, res: Response) => {
+    const emailData = InvitationSendEmailSchema.parse(req.body);
+    const sendingEmail = await InvitationService.sendInvitation(
+      emailData.receiverName,
+      emailData.receiverEmail,
+      emailData.senderId
+    );
+    res.status(200).json({ data: sendingEmail });
+  },
+
+  revokeInvitation: async (req: Request, res: Response) => {
+    const { invitationId } = InvitationIdSchema.parse(req.params);
+    const revokedInvitation = InvitationService.revokeInvitation(invitationId);
+    res.status(200).json({ data: revokedInvitation });
+  },
+
+  validateInvitation: async (req: Request, res: Response) => {
+    const { token } = InvitationTokenSchema.parse(req.body);
+    const foundInvitation = InvitationService.validateInvitation(token);
+    res.status(200).json({ data: foundInvitation });
+  },
+};
