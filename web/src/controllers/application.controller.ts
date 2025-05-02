43ef7c966fde0e7ef63e9f5d5ef44dcbf7863097
dsmai <son.dmai@gmail.com>
2025-05-02 14:26:15 -0400

    Added repository unit tests

--- web/src/controllers/application.controller.ts
+++ web/src/controllers/application.controller.ts
@@ -151,12 +151,12 @@ export const ApplicationController = {
   getApplicationsCountByStatus: async (req: Request, res: Response) => {
     const userId = getUserFromRequest(req).user.id;
 
-    const applicationsCount =
+    const applicationsCountByStatus =
       await ApplicationService.getApplicationsCountByStatus(userId);
 
     res.status(200).json({
-      message: 'Applications count retrieved successfully',
-      data: applicationsCount,
+      message: 'Applications count by status retrieved successfully',
+      data: applicationsCountByStatus,
     });
   },
 };
