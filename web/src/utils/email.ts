93c6aa672f76fe5c4a8defd7e43271d16daf0f3e
Nam Nguyen <114103526+Euclid0192@users.noreply.github.com>
2025-05-05 00:32:25 -0400

    [Authentication] Invitations Service Layer (#111)
    Co-authored-by: Duy Kha <duykha121314888@gmail.com>

---
+++ web/src/utils/email.ts
@@ -0,0 +1,50 @@
+import { EnvConfig } from '@/config/env';
+import nodemailer from 'nodemailer';
+
+export class EmailService {
+  private transporter: nodemailer.Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      auth: {
+        user: EnvConfig.get().GMAIL_EMAIL,
+        pass: EnvConfig.get().GMAIL_APP_PASSWORD,
+      },
+    });
+  }
+
+  getInvitationEmailTemplate(name: string, email: string, token: string) {
+    const link = `${EnvConfig.get().VTMP_WEB_URL}?token=${token}`;
+    return {
+      email,
+      subject: 'Invitation to join VTMP',
+      body: `<h1>Invitation to join VTMP</h1>
+        <p>Dear ${name},</p>
+        <p>We are excited to invite you to join our VTMP community. As a member, you will have access to exclusive content, resources, and networking opportunities.</p>
+        <p>To accept this invitation, please click the link below:</p>
+        <a href=${link} target="_blank">Join the community!</a>
+        `,
+    };
+  }
+
+  async sendEmail({
+    email,
+    subject,
+    body,
+  }: {
+    email: string;
+    subject: string;
+    body: string;
+  }) {
+    const mailOptions = {
+      from: EnvConfig.get().GMAIL_EMAIL,
+      to: email,
+      subject,
+      html: body,
+    };
+
+    await this.transporter.sendMail(mailOptions);
+  }
+}
