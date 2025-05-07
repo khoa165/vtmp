cf13f9d87bcaf3c61f8271695a7069ec86357ada
QuangMinhNguyen27405 <quangminh27405@gmail.com>
2025-05-07 11:06:43 -0500

    Finalized tests

--- web/src/services/interview.service.test.ts
+++ web/src/services/interview.service.test.ts
@@ -7,6 +7,7 @@ import { useMongoDB } from '@/testutils/mongoDB.testutil';
 import { getNewMongoId, toMongoId } from '@/testutils/mongoID.testutil';
 import { InterviewStatus, InterviewType } from '@vtmp/common/constants';
 import { ResourceNotFoundError } from '@/utils/errors';
+import { differenceInSeconds } from 'date-fns';
 
 describe('InterviewService', () => {
   useMongoDB();
@@ -314,6 +315,80 @@ describe('InterviewService', () => {
         userId: toMongoId(mockInterview_A0.userId),
         status: InterviewStatus.PASSED,
       });
+    });
+  });
+
+  describe('deleteInterviewById', () => {
+    it('should throw error of the interview does not exists', async () => {
+      await expect(
+        InterviewService.deleteInterviewById({
+          interviewId: getNewMongoId(),
+          userId: userId_A,
+        })
+      ).eventually.to.be.rejectedWith(
+        ResourceNotFoundError,
+        'Interview not found'
+      );
+    });
+
+    it('should throw error of the interview does not belong to the authorized user', async () => {
+      const interview_A0 =
+        await InterviewRepository.createInterview(mockInterview_A0);
+
+      await expect(
+        InterviewService.deleteInterviewById({
+          interviewId: interview_A0.id,
+          userId: userId_B,
+        })
+      ).eventually.to.be.rejectedWith(
+        ResourceNotFoundError,
+        'Interview not found'
+      );
+    });
+
+    it('should throw error of the interview is already soft deleted', async () => {
+      const interview_A1 =
+        await InterviewRepository.createInterview(mockInterview_A1);
+
+      await InterviewRepository.deleteInterviewById({
+        interviewId: interview_A1.id,
+        userId: userId_A,
+      });
+
+      await expect(
+        InterviewService.deleteInterviewById({
+          interviewId: interview_A1.id,
+          userId: userId_A,
+        })
+      ).eventually.to.be.rejectedWith(
+        ResourceNotFoundError,
+        'Interview not found'
+      );
+    });
+
+    it('should soft delete the interview', async () => {
+      const interview_A0 =
+        await InterviewRepository.createInterview(mockInterview_A0);
+
+      const deletedInterview = await InterviewService.deleteInterviewById({
+        interviewId: interview_A0.id,
+        userId: userId_A,
+      });
+
+      const foundInterview = await InterviewRepository.getInterviewById({
+        interviewId: interview_A0.id,
+        userId: userId_A,
+      });
+
+      assert(deletedInterview);
+      assert(!foundInterview);
+      expect(deletedInterview.id).to.equal(interview_A0.id);
+      assert(deletedInterview.deletedAt);
+      const timeDiff = differenceInSeconds(
+        new Date(),
+        deletedInterview.deletedAt
+      );
+      expect(timeDiff).to.lessThan(3);
     });
   });
 });
--- web/src/services/interview.service.ts
+++ web/src/services/interview.service.ts
@@ -1,6 +1,5 @@
 import { InterviewRepository } from '@/repositories/interview.repository';
 import { IInterview } from '@/models/interview.model';
-import { UpdateResult } from 'mongoose';
 
 import { InterviewStatus, InterviewType } from '@vtmp/common/constants';
 import { ResourceNotFoundError } from '@/utils/errors';
@@ -37,7 +36,7 @@ export const InterviewService = {
   }: {
     interviewId: string;
     userId: string;
-  }): Promise<IInterview | null> => {
+  }): Promise<IInterview> => {
     const interview = await InterviewRepository.getInterviewById({
       interviewId,
       userId,
@@ -79,28 +78,21 @@ export const InterviewService = {
       interviewOnDate?: Date;
       note?: string;
     };
-  }): Promise<IInterview | null> => {
-    return InterviewRepository.updateInterviewById({
+  }): Promise<IInterview> => {
+    const updatedInterview = await InterviewRepository.updateInterviewById({
       interviewId,
       userId,
       updatedMetadata,
     });
-  },
 
-  updateInterviewsWithStatus: async ({
-    userId,
-    interviewIds,
-    updatedStatus,
-  }: {
-    userId: string;
-    interviewIds: string[];
-    updatedStatus: InterviewStatus;
-  }): Promise<UpdateResult> => {
-    return InterviewRepository.updateInterviewsWithStatus({
-      userId,
-      interviewIds,
-      updatedStatus,
-    });
+    if (!updatedInterview) {
+      throw new ResourceNotFoundError('Interview not found', {
+        interviewId,
+        userId,
+      });
+    }
+
+    return updatedInterview;
   },
 
   deleteInterviewById: async ({
@@ -109,7 +101,19 @@ export const InterviewService = {
   }: {
     interviewId: string;
     userId: string;
-  }): Promise<IInterview | null> => {
-    return InterviewRepository.deleteInterviewById({ interviewId, userId });
+  }): Promise<IInterview> => {
+    const deletedInterview = await InterviewRepository.deleteInterviewById({
+      interviewId,
+      userId,
+    });
+
+    if (!deletedInterview) {
+      throw new ResourceNotFoundError('Interview not found', {
+        interviewId,
+        userId,
+      });
+    }
+
+    return deletedInterview;
   },
 };
