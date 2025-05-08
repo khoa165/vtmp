bcc7f94b3118f2f5842acfafa8a356aab6f4308c
dsmai <son.dmai@gmail.com>
2025-05-07 22:12:12 -0400

    Modified request() function to allow dynamically resolving return type bases on options

--- web-client/src/utils/api.ts
+++ web-client/src/utils/api.ts
@@ -8,21 +8,55 @@ const api = axios.create({
   },
 });
 
-export const request = async <T>(
-  method: Method.GET | Method.POST | Method.DELETE | Method.PUT,
-  url: string,
-  data?: unknown,
-  schema?: { parse: (data: unknown) => T }
-): Promise<T> => {
+// Base interface
+interface RequestBaseArgs<T> {
+  method: Method.GET | Method.POST | Method.DELETE | Method.PUT;
+  url: string;
+  data?: object;
+  schema: { parse: (data: object) => T };
+}
+
+// Define an interface that defines 2 overloads for the request function
+// Overloads allow function to have multiple call signature, depending on arguments passed to it
+interface IRequest {
+  // First overload: if options.includeOnlyData is true
+  // Return only the data field from parsed response
+  <T extends { data: object; message: string }>(
+    args: RequestBaseArgs<T> & { options: { includeOnlyDataField: true } }
+  ): Promise<T['data']>;
+
+  // Second overload: if options.includeOnlyData is false or not provided
+  // Return the entire parsed response object
+  <T extends { data: object; message: string }>(
+    args: RequestBaseArgs<T> & {
+      options?: { includeOnlyDataField?: false | undefined };
+    }
+  ): Promise<T>;
+}
+
+// Implementation of the function using the interface IRequest
+export const request: IRequest = async <
+  T extends { data: object; message: string },
+>({
+  method,
+  url,
+  data = {},
+  schema,
+  options = {},
+}: {
+  method: Method.GET | Method.POST | Method.DELETE | Method.PUT;
+  url: string;
+  data?: object;
+  schema: { parse: (data: object) => T };
+  options?: { includeOnlyDataField?: boolean };
+}): Promise<T['data'] | T> => {
+  const { includeOnlyDataField = false } = options;
   const response = await api.request({
     method,
     url,
-    ...(method === Method.GET && data ? { params: data } : {}), // Use `params` for GET requests
-    ...(method !== Method.GET && data ? { data } : {}), // Use `data` for non-GET requests
+    ...(method === Method.GET ? { params: data } : { data }),
   });
-  if (schema) {
-    return schema.parse(response.data);
-  }
+  const parsedData = schema.parse(response.data);
 
-  return response.data;
+  return includeOnlyDataField ? parsedData.data : parsedData;
 };
