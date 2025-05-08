23fe46e2a39fbfea88e15b14d90271045ccdae25
dsmai <son.dmai@gmail.com>
2025-05-08 15:19:04 -0400

    Cleaned up application column

--- web-client/src/components/pages/application-tracker/applications/applications-table-columns.tsx
+++ web-client/src/components/pages/application-tracker/applications/applications-table-columns.tsx
@@ -1,53 +1,13 @@
 import { ColumnDef } from '@tanstack/react-table';
 import { Checkbox } from '@/components/base/checkbox';
-import { ArrowUpDown, ChevronDown } from 'lucide-react';
-import { Button } from '@/components/base/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/base/dropdown-menu';
 import { IApplication } from '@/components/pages/application-tracker/applications/validation';
 import { ApplicationStatus } from '@vtmp/common/constants';
-import { ApplicationsAction } from '@/components/pages/application-tracker/applications/applications-action';
+import {
+  CellActions,
+  CellApplicationStatus,
+} from '@/components/pages/application-tracker/applications/cell';
 import { format } from 'date-fns';
-import { titleCase } from '@/utils/helpers';
-
-import confetti from 'canvas-confetti';
-
-const handleClick = () => {
-  const end = Date.now() + 3 * 1000; // 3 seconds
-  const colors = ['#a786ff', '#fd8bbc', '#eca184', '#f8deb1'];
-
-  const frame = () => {
-    if (Date.now() > end) return;
-
-    confetti({
-      particleCount: 2,
-      angle: 60,
-      spread: 55,
-      startVelocity: 60,
-      origin: { x: 0, y: 0.5 },
-      colors: colors,
-    });
-    confetti({
-      particleCount: 2,
-      angle: 120,
-      spread: 55,
-      startVelocity: 60,
-      origin: { x: 1, y: 0.5 },
-      colors: colors,
-    });
-
-    requestAnimationFrame(frame);
-  };
-
-  frame();
-};
-
-// TODO-(QuangMinhNguyen27405/dsmai): General => Add arrow up when sorting ascending and down when descending
-// and updown if we are sorting by a different column
+import { HeaderSorting } from '@/components/pages/application-tracker/applications/header';
 
 export const applicationsTableColumns = ({
   deleteApplicationFn,
@@ -86,73 +46,29 @@ export const applicationsTableColumns = ({
   },
   {
     accessorKey: 'companyName',
-    header: 'Company',
+    header: ({ column }) => {
+      return <HeaderSorting column={column} headerName="Company" />;
+    },
   },
   {
     accessorKey: 'status',
     header: ({ column }) => {
-      // TODO-(QuangMinhNguyen27405/dsmai): Add arrow up when sorting ascending and down when descending
-      // and updown if we are sorting by a different column
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Status
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
+      return <HeaderSorting column={column} headerName="Status" />;
     },
     cell: ({ row }) => {
       const application = row.original;
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Button variant="outline">
-              {application.status === ApplicationStatus.OA
-                ? application.status
-                : titleCase(application.status)}{' '}
-              <ChevronDown />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            {Object.values(ApplicationStatus).map((dropdownStatus, index) => (
-              <DropdownMenuItem
-                key={index}
-                onClick={() => {
-                  if (dropdownStatus === ApplicationStatus.OFFERED) {
-                    handleClick();
-                  }
-                  updateApplicationStatusFn({
-                    applicationId: application._id,
-                    body: { updatedStatus: dropdownStatus },
-                  });
-                }}
-              >
-                {dropdownStatus === ApplicationStatus.OA
-                  ? dropdownStatus
-                  : titleCase(dropdownStatus)}
-              </DropdownMenuItem>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <CellApplicationStatus
+          application={application}
+          updateApplicationStatusFn={updateApplicationStatusFn}
+        />
       );
     },
   },
   {
     accessorKey: 'appliedOnDate',
     header: ({ column }) => {
-      // TODO-(QuangMinhNguyen27405/dsmai): Add arrow up when sorting ascending and down when descending
-      // and updown if we are sorting by a different column
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Date Applied
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
+      return <HeaderSorting column={column} headerName="Date Applied" />;
     },
     cell: ({ row }) => {
       const isoDate = row.getValue<string>('appliedOnDate');
@@ -172,7 +88,7 @@ export const applicationsTableColumns = ({
     cell: ({ row }) => {
       const application = row.original;
       return (
-        <ApplicationsAction
+        <CellActions
           application={application}
           deleteApplicationFn={deleteApplicationFn}
         />
--- web-client/src/components/pages/application-tracker/applications/cell.tsx
+++ web-client/src/components/pages/application-tracker/applications/cell.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/base/button';
-import { MoreHorizontal } from 'lucide-react';
+import { MoreHorizontal, ChevronDown } from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,8 +19,41 @@ import {
 } from '@/components/base/alert-dialog';
 import { IApplication } from '@/components/pages/application-tracker/applications/validation';
 import { useState } from 'react';
+import confetti from 'canvas-confetti';
+import { ApplicationStatus } from '@vtmp/common/constants';
+import { titleCase } from '@/utils/helpers';
 
-export const ApplicationsAction = ({
+const handleOfferedSelected = () => {
+  const end = Date.now() + 3 * 1000; // 3 seconds
+  const colors = ['#a786ff', '#fd8bbc', '#eca184', '#f8deb1'];
+
+  const frame = () => {
+    if (Date.now() > end) return;
+
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      startVelocity: 60,
+      origin: { x: 0, y: 0.5 },
+      colors: colors,
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      startVelocity: 60,
+      origin: { x: 1, y: 0.5 },
+      colors: colors,
+    });
+
+    requestAnimationFrame(frame);
+  };
+
+  frame();
+};
+
+export const CellActions = ({
   application,
   deleteApplicationFn,
 }: {
@@ -28,7 +61,6 @@ export const ApplicationsAction = ({
   deleteApplicationFn: (id: string) => void;
 }) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-
   return (
     <>
       <DropdownMenu>
@@ -76,6 +108,55 @@ export const ApplicationsAction = ({
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
+    </>
+  );
+};
+
+export const CellApplicationStatus = ({
+  application,
+  updateApplicationStatusFn,
+}: {
+  application: IApplication;
+  updateApplicationStatusFn: ({
+    applicationId,
+    body,
+  }: {
+    applicationId: string;
+    body: { updatedStatus: ApplicationStatus };
+  }) => void;
+}) => {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="outline">
+            {application.status === ApplicationStatus.OA
+              ? application.status
+              : titleCase(application.status)}{' '}
+            <ChevronDown />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {Object.values(ApplicationStatus).map((dropdownStatus, index) => (
+            <DropdownMenuItem
+              key={index}
+              onClick={() => {
+                if (dropdownStatus === ApplicationStatus.OFFERED) {
+                  handleOfferedSelected();
+                }
+                updateApplicationStatusFn({
+                  applicationId: application._id,
+                  body: { updatedStatus: dropdownStatus },
+                });
+              }}
+            >
+              {dropdownStatus === ApplicationStatus.OA
+                ? dropdownStatus
+                : titleCase(dropdownStatus)}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
