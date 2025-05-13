a2265ce5c99a918305a76d279e1fefb436b2f2e6
Hong An Tran <hongantran3804@gmail.com>
2025-05-13 08:33:03 -0700

    Update link sharing controller and controller test

--- web/src/controllers/link.controller.ts
+++ web/src/controllers/link.controller.ts
@@ -76,7 +76,7 @@ export const LinkController = {
     const submitLink = await LinkService.submitLink(parsedLink.url);
     res.status(201).json({
       message: 'Link has been submitted successfully.',
-      data: { link: submitLink },
+      data: submitLink,
     });
   },
 
@@ -84,7 +84,7 @@ export const LinkController = {
     const filters = LinkFilterSchema.parse(req.query);
     const links = await LinkService.getLinks(filters);
     res.status(200).json({
-      messsage: 'Links have been retrieved successfully.',
+      message: 'Links have been retrieved successfully.',
       data: links,
     });
   },
@@ -107,7 +107,7 @@ export const LinkController = {
     );
     res.status(200).json({
       message: 'Link has been approved!',
-      data: { link: approvedLink },
+      data: approvedLink,
     });
   },
 
@@ -117,7 +117,7 @@ export const LinkController = {
     const rejectedLink = await LinkService.rejectLink(parsedLink.linkId);
     res.status(200).json({
       message: 'Link has been rejected!',
-      data: { link: rejectedLink },
+      data: rejectedLink,
     });
   },
 };
