d2e02b02bf4b0e15d91f189578b46a8126d9a796
dsmai <son.dmai@gmail.com>
2025-05-14 02:55:33 -0400

    Refactored skeleton for status container

--- web-client/src/components/pages/application-tracker/applications/application-status-card.tsx
+++ web-client/src/components/pages/application-tracker/applications/application-status-card.tsx
@@ -1,11 +1,9 @@
 import { Card } from '@/components/base/card';
 import { ApplicationStatus } from '@vtmp/common/constants';
-import { Skeleton } from '@/components/base/skeleton';
 import { StatusDot } from '@/components/base/status-dot';
 import { formatStatus } from '@/utils/helpers';
 
 export const ApplicationStatusCard = ({
-  isSkeleton = false,
   status,
   count,
   onClick,
@@ -27,21 +25,10 @@ export const ApplicationStatusCard = ({
       onClick={onClick}
     >
       <section className="flex flex-col items-center justify-center">
-        <div className="text-[2rem] max-lg:text-[1rem] font-bold">
-          {isSkeleton ? (
-            <Skeleton className="w-[2rem] h-[2rem] max-lg:w-[1.5rem] max-lg:h-[1.5rem] rounded-md" />
-          ) : (
-            count
-          )}
-        </div>
+        <div className="text-[2rem] max-lg:text-[1rem] font-bold">{count}</div>
 
         <div className="flex flex-row items-center gap-2">
-          {isSkeleton ? (
-            <>
-              <Skeleton className="w-[1rem] h-[1rem] max-lg:w-[0.7rem] max-lg:h-[0.7rem] rounded-full" />
-              <Skeleton className="w-[4rem] h-[1rem] max-lg:w-[3rem] max-lg:h-[0.8rem] rounded-md" />
-            </>
-          ) : status ? (
+          {status ? (
             <>
               <StatusDot status={status} />
               <span className="font-bold max-lg:text-[0.7rem] text-wrap">
--- web-client/src/components/pages/application-tracker/applications/application-status-container.tsx
+++ web-client/src/components/pages/application-tracker/applications/application-status-container.tsx
@@ -2,6 +2,7 @@ import { useGetApplicationsCountByStatus } from '@/components/pages/application-
 import { ApplicationStatus } from '@vtmp/common/constants';
 import { useState } from 'react';
 import { ApplicationStatusCard } from '@/components/pages/application-tracker/applications/application-status-card';
+import { Skeleton } from '@/components/base/skeleton';
 
 export const ApplicationStatusContainer = ({
   setFilter,
@@ -19,8 +20,8 @@ export const ApplicationStatusContainer = ({
   if (isLoading) {
     return (
       <div className="grid grid-cols-5 w-full gap-4 mb-6 max-md:grid-rows-6">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <ApplicationStatusCard key={index} isSkeleton={true} />
+        {Array.from({ length: 5 }).map(() => (
+          <Skeleton className="w-full h-[8rem] rounded-xl" />
         ))}
       </div>
     );
@@ -50,31 +51,6 @@ export const ApplicationStatusContainer = ({
   };
 
   return (
-    // <div className={`grid grid-cols-5 w-full gap-4 mb-6 max-md:grid-rows-6`}>
-    //   {allDisplayedStatus.map((displayedStatus, index) => (
-    //     <Card
-    //       key={index}
-    //       className={`active:bg-gray-300 bg-transparent h-fit cursor-pointer transition-colors duration-200 ${
-    //         selectedStatus === displayedStatus
-    //           ? 'bg-[#FEFEFE] text-[#333333]'
-    //           : 'hover:bg-[#FEFEFE] hover:text-[#333333]'
-    //       }`}
-    //       onClick={() => handleStatusClick(displayedStatus)}
-    //     >
-    //       <section className="flex flex-col items-center justify-center">
-    //         <div className="text-[2rem] max-lg:text-[1rem] font-bold">
-    //           {ApplicationsCountByStatus?.[displayedStatus]}
-    //         </div>
-    //         <div className="flex flex-row items-center gap-2">
-    //           <StatusDot status={displayedStatus} />
-    //           <span className="font-bold max-lg:text-[0.7rem] text-wrap">
-    //             {formatStatus(displayedStatus)}
-    //           </span>
-    //         </div>
-    //       </section>
-    //     </Card>
-    //   ))}
-    // </div>
     <div className="grid grid-cols-5 w-full gap-4 mb-6 max-md:grid-rows-6">
       {allDisplayedStatus.map((displayedStatus, index) => (
         <ApplicationStatusCard
