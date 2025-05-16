4494651fa800273e6234d59523451a15a923e8b1
Dang Son Mai <son.dmai@gmail.com>
2025-05-16 16:42:13 -0400

    Application tracker dashboard - Added interactive status cards and filtering by status (#159)
    Co-authored-by: Hong An Tran <hongantran3804@gmail.com>
    Co-authored-by: QuangMinhNguyen27405 <quangminh27405@gmail.com>
    Co-authored-by: Khoa Le <khoatle165@gmail.com>

--- web-client/src/components/pages/application-tracker/applications/applications-page.tsx
+++ web-client/src/components/pages/application-tracker/applications/applications-page.tsx
@@ -1,13 +1,19 @@
-import { ApplicationStatusCards } from '@/components/pages/application-tracker/applications/application-status-cards';
+import { ApplicationStatusContainer } from '@/components/pages/application-tracker/applications/application-status-container';
 import { ApplicationsContainer } from '@/components/pages/application-tracker/applications/applications-containter';
+import { ApplicationStatus } from '@vtmp/common/constants';
 import { useState } from 'react';
 
+export interface ApplicationsFilter {
+  status?: ApplicationStatus;
+}
+
 export const ApplicationsPage = () => {
-  const [filter, setFilter] = useState({});
+  const [applicationFilter, setApplicationFilter] =
+    useState<ApplicationsFilter>({});
   return (
     <div className="w-full h-full p-10">
-      <ApplicationStatusCards setFilter={setFilter} />
-      <ApplicationsContainer filter={filter} />
+      <ApplicationStatusContainer setApplicationFilter={setApplicationFilter} />
+      <ApplicationsContainer applicationFilter={applicationFilter} />
     </div>
   );
 };
