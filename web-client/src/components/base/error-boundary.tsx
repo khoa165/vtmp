25dbeaab1983be3eb19a4819dfec2da474d80cb0
dsmai <son.dmai@gmail.com>
2025-05-18 18:18:04 -0400

    Added another button to Go Home

--- web-client/src/components/base/error-boundary.tsx
+++ web-client/src/components/base/error-boundary.tsx
@@ -2,6 +2,7 @@ import { Alert, AlertDescription, AlertTitle } from '@/components/base/alert';
 import { Button } from '@/components/base/button';
 import { ErrorBoundary } from 'react-error-boundary';
 import { handleError } from '@/utils/errors';
+import { useNavigate } from 'react-router-dom';
 
 export const ErrorBoundaryWrapper = ({
   customText,
@@ -28,17 +29,25 @@ const ErrorBoundaryFallback = ({
 }: {
   resetErrorBoundary: () => void;
   customText: string;
-}) => (
-  <Alert
-    variant="default"
-    className="flex flex-col justify-center p-4 bg-transparent border border-white rounded-md gap-4 mb-6"
-  >
-    <AlertTitle className="text-2xl">Something went wrong</AlertTitle>
-    <AlertDescription>
-      Sorry, there was an error loading {customText}. Please try again.
-    </AlertDescription>
-    <Button onClick={resetErrorBoundary} variant="outline">
-      Try Again
-    </Button>
-  </Alert>
-);
+}) => {
+  const navigate = useNavigate();
+  return (
+    <Alert
+      variant="default"
+      className="flex flex-col justify-center p-4 bg-transparent border border-white rounded-md gap-4 mb-6"
+    >
+      <AlertTitle className="text-2xl">Something went wrong</AlertTitle>
+      <AlertDescription>
+        Sorry, there was an error loading {customText}. Please try again.
+      </AlertDescription>
+      <section className="flex gap-2">
+        <Button onClick={resetErrorBoundary} variant="outline">
+          Try Again
+        </Button>
+        <Button onClick={() => navigate('/')} variant="outline">
+          Go Home
+        </Button>
+      </section>
+    </Alert>
+  );
+};
