ff49f39afd2cae51ceb5b52621a0368473c24a9f
Dang Son Mai <son.dmai@gmail.com>
2025-05-20 01:44:36 -0400

    Added react-error-boundary (#178)
    Co-authored-by: Hong An Tran <hongantran3804@gmail.com>
    Co-authored-by: QuangMinhNguyen27405 <quangminh27405@gmail.com>
    Co-authored-by: Khoa Le <khoatle165@gmail.com>

--- web-client/src/components/pages/application-tracker/applications/header.tsx
+++ web-client/src/components/pages/application-tracker/applications/header.tsx
@@ -1,16 +1,20 @@
 import { ArrowUp, ArrowDown, ArrowUpDown } from 'lucide-react';
 import { Button } from '@/components/base/button';
 
+type SortingState = 'asc' | 'desc';
+type ArrowIcon = typeof ArrowUp | typeof ArrowDown;
+
+const SortingStateToIconMapping: Record<SortingState, ArrowIcon> = {
+  asc: ArrowUp,
+  desc: ArrowDown,
+};
+
 export const HeaderSorting = ({ column, headerName }) => {
   const sortingState = column.getIsSorted();
-  let SortingIcon;
-  if (sortingState == 'asc') {
-    SortingIcon = ArrowUp;
-  } else if (sortingState === 'desc') {
-    SortingIcon = ArrowDown;
-  } else {
-    SortingIcon = ArrowUpDown;
-  }
+  const SortingIcon =
+    sortingState && SortingStateToIconMapping[sortingState]
+      ? SortingStateToIconMapping[sortingState]
+      : ArrowUpDown;
 
   return (
     <div className="flex items-center">
