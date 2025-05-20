92cefe929d9d17d5b24f2f7e2465800ce00dcda7
Hong An Tran <hongantran3804@gmail.com>
2025-05-19 22:51:10 -0700

    move header component from application-tracker to base

---
+++ web-client/src/components/base/header.tsx
@@ -0,0 +1,28 @@
+import { ArrowUp, ArrowDown, ArrowUpDown } from 'lucide-react';
+import { Button } from '@/components/base/button';
+
+export const HeaderSorting = ({ column, headerName }) => {
+  const sortingState = column.getIsSorted();
+  let SortingIcon;
+  if (sortingState == 'asc') {
+    SortingIcon = ArrowUp;
+  } else if (sortingState === 'desc') {
+    SortingIcon = ArrowDown;
+  } else {
+    SortingIcon = ArrowUpDown;
+  }
+
+  return (
+    <div className="flex items-center">
+      <span>{headerName}</span>
+      <Button
+        variant="outline"
+        className="p-0 ml-0 w-fit hover:text-black cursor-pointer"
+        size="sm"
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
+        <SortingIcon className="h-4 w-4" />
+      </Button>
+    </div>
+  );
+};
