6929fc2b88e5c07661bd8e9a6c1957a79b724d55
Nam Nguyen <nguy1053@msu.edu>
2025-05-20 22:46:33 -0400

    Addresing PR comment

--- web-client/src/components/layout/page-with-sidebar.tsx
+++ web-client/src/components/layout/page-with-sidebar.tsx
@@ -1,29 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { InternalToolsSidebar } from '@/components/layout/internal-tools-sidebar';
 import { SidebarProvider } from '@/components/base/sidebar';
 import { useNavigatePreserveQueryParams } from '@/hooks/useNavigatePreserveQueryParams';
 import { toast } from 'sonner';
-import Redirecting from '@/components/base/redirect';
+
 export const PageWithSidebar = () => {
   const open = document.cookie.split('=')[1];
   const state = open === 'true';
 
   const token = localStorage.getItem('token');
   const navigate = useNavigatePreserveQueryParams();
-  const [isRedirecting, setIsRedirecting] = useState(false);
 
   useEffect(() => {
     if (!token) {
-      setIsRedirecting(true);
       navigate('/login');
       toast('Please login to your account to continue');
     }
   }, [token]);
-
-  if (isRedirecting) {
-    return <Redirecting />;
-  }
 
   return (
     <SidebarProvider defaultOpen={state}>
