5ea3d330936ba4e2777ca8a3c34d82b9b8cb2dc9
Hong An Tran <hongantran3804@gmail.com>
2025-05-23 10:09:30 -0700

    Update add new function getLinkByUrl for deduplication and test for service, repo layer

---
+++ web/src/services/link/link-deduplicator.service.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import { useMongoDB } from '@/testutils/mongoDB.testutil';
+import { LinkRepository } from '@/repositories/link.repository';
+import { ILink } from '@/models/link.model';
+import { LinkDeduplicatorService } from '@/services/link/link-deduplicator.service';
+
+describe('LinkDeduplicatorService', () => {
+  useMongoDB();
+
+  describe('checkDuplicate', () => {
+    it('should throw error when link already exists', async () => {
+      const mockLinkData = {
+        url: 'google.com',
+        jobTitle: 'Software Engineer',
+        companyName: 'Example Company',
+      };
+      const googleLink: ILink = await LinkRepository.createLink(mockLinkData);
+      const newLink = {
+        url: googleLink.url,
+        jobTitle: 'Software Engineer',
+        companyName: 'Example Company',
+      };
+
+      await expect(
+        LinkDeduplicatorService.checkDuplicate(newLink.url)
+      ).to.be.rejectedWith('Duplicate link found');
+    });
+
+    it('should not throw error when link does not exist', async () => {
+      await expect(
+        LinkDeduplicatorService.checkDuplicate('nonexistent.com')
+      ).to.not.be.rejectedWith('Duplicate link found');
+    });
+  });
+});
