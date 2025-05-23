67ddbd6be05933f332ecb9c6fd77388ef7bffed3
Hong An Tran <hongantran3804@gmail.com>
2025-05-23 09:42:48 -0700

    Delete link-deduplicator repo layer

--- web/src/repositories/link/link-deduplicator.repository.ts
+++
@@ -1,7 +0,0 @@
-import { LinkModel, ILink } from '@/models/link.model';
-
-export class LinkDeduplicatorRepo {
-  static async findLinkByUrl(url: string): Promise<ILink | null> {
-    return LinkModel.findOne({ where: { url } }).lean();
-  }
-}
