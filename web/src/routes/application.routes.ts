d705f479883f41114ef6791d529746ae18b5b24d
dsmai <son.dmai@gmail.com>
2025-05-22 23:21:46 -0400

    Fixed routes

--- web/src/routes/application.routes.ts
+++ web/src/routes/application.routes.ts
@@ -7,35 +7,54 @@ import { Permission } from '@vtmp/common/constants';
 
 export const ApplicationRoutes = Router();
 
-ApplicationRoutes.use(
-  wrappedHandlers([authenticate, hasPermission(Permission.MANAGE_APPLICATION)])
-);
+ApplicationRoutes.use(wrappedHandlers([authenticate]));
 
-ApplicationRoutes.post(
-  '/',
-  wrappedHandlers([ApplicationController.createApplication])
-);
 ApplicationRoutes.get(
   '/',
-  wrappedHandlers([ApplicationController.getApplications])
+  wrappedHandlers([
+    hasPermission(Permission.VIEW_APPLICATION),
+    ApplicationController.getApplications,
+  ])
 );
 ApplicationRoutes.get(
   '/count-by-status',
-  wrappedHandlers([ApplicationController.getApplicationsCountByStatus])
+  wrappedHandlers([
+    hasPermission(Permission.VIEW_APPLICATION),
+    ApplicationController.getApplicationsCountByStatus,
+  ])
 );
 ApplicationRoutes.get(
   '/:applicationId',
-  wrappedHandlers([ApplicationController.getApplicationById])
+  wrappedHandlers([
+    hasPermission(Permission.VIEW_APPLICATION),
+    ApplicationController.getApplicationById,
+  ])
+);
+ApplicationRoutes.post(
+  '/',
+  wrappedHandlers([
+    hasPermission(Permission.MANAGE_APPLICATION),
+    ApplicationController.createApplication,
+  ])
 );
 ApplicationRoutes.put(
   '/:applicationId/updateStatus',
-  wrappedHandlers([ApplicationController.updateApplicationStatus])
+  wrappedHandlers([
+    hasPermission(Permission.MANAGE_APPLICATION),
+    ApplicationController.updateApplicationStatus,
+  ])
 );
 ApplicationRoutes.put(
   '/:applicationId',
-  wrappedHandlers([ApplicationController.updateApplicationMetadata])
+  wrappedHandlers([
+    hasPermission(Permission.MANAGE_APPLICATION),
+    ApplicationController.updateApplicationMetadata,
+  ])
 );
 ApplicationRoutes.delete(
   '/:applicationId',
-  wrappedHandlers([ApplicationController.deleteApplication])
+  wrappedHandlers([
+    hasPermission(Permission.MANAGE_APPLICATION),
+    ApplicationController.deleteApplication,
+  ])
 );
--- web/src/routes/job-posting.routes.ts
+++ web/src/routes/job-posting.routes.ts
@@ -2,8 +2,6 @@ import { Router } from 'express';
 import { JobPostingController } from '@/controllers/job-posting.controller';
 import { wrappedHandlers } from '@/middlewares/utils';
 import { authenticate } from '@/middlewares/auth.middleware';
-import { hasPermission } from '@/middlewares/authorization.middleware';
-import { Permission } from '@vtmp/common/constants';
 
 export const JobPostingRoutes = Router();
 
@@ -17,9 +15,6 @@ JobPostingRoutes.delete(
 );
 JobPostingRoutes.get(
   '/not-applied',
-  wrappedHandlers([
-    authenticate,
-    hasPermission(Permission.MANAGE_APPLICATION),
-    JobPostingController.getJobPostingsUserHasNotAppliedTo,
-  ])
+  authenticate,
+  wrappedHandlers([JobPostingController.getJobPostingsUserHasNotAppliedTo])
 );
