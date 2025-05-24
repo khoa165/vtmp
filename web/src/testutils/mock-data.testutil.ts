113372d959b3bacfc6188e7e7f87efd757ef4325
Khoa Le <khoatle165@gmail.com>
2025-05-24 05:42:34 -0400

    Fix build

--- web/src/testutils/mock-data.testutil.ts
+++ web/src/testutils/mock-data.testutil.ts
@@ -1,3 +1,5 @@
+import { ENVIRONMENT } from '@/constants/enums';
+
 export const MOCK_ENV = {
   PORT: 8000,
   MONGO_URI: 'mongodb://username:password@localhost:27017/database_name',
@@ -5,5 +7,5 @@ export const MOCK_ENV = {
   GMAIL_EMAIL: 'vtmpwebsite2025@gmail.com',
   GMAIL_APP_PASSWORD: 'azpj ibvt glaf ebcy',
   VTMP_WEB_URL: 'viettechmentorship.com',
-  SEED_ENV: 'ENV',
+  SEED_ENV: ENVIRONMENT.DEV,
 };
