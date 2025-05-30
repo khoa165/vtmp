ab7d318c4a85694ca52a9354c8301c4060c514c9
dsmai <son.dmai@gmail.com>
2025-05-29 21:04:52 -0400

    Changed port

--- discord-service/src/app.ts
+++ discord-service/src/app.ts
@@ -5,15 +5,20 @@ import { EnvConfig } from '@/config/env';
 
 const app: Express = express();
 
+app.use(express.json());
+
 const creator = new SlashCreator({
   applicationID: EnvConfig.get().DISCORD_APPLICATION_ID,
   publicKey: EnvConfig.get().DISCORD_PUBLIC_KEY,
   token: EnvConfig.get().DISCORD_BOT_TOKEN,
 });
 
-creator
-  .withServer(new ExpressServer(app))
-  .registerCommands([new PingCommand(creator), new ShareLinkCommand(creator)]);
+creator.withServer(new ExpressServer(app));
+
+creator.registerCommands([
+  new PingCommand(creator),
+  new ShareLinkCommand(creator),
+]);
 
 creator.syncCommands();
 
--- discord-service/src/server.ts
+++ discord-service/src/server.ts
@@ -1,7 +1,7 @@
 import app from '@/app';
 import { EnvConfig } from '@/config/env';
 
-const PORT = EnvConfig.get().PORT || 8080;
+const PORT = EnvConfig.get().PORT || 80;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
