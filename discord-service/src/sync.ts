ad7a5be30a7ae693ba01ee8f3a58f1d7d5133239
dsmai <son.dmai@gmail.com>
2025-05-29 22:14:26 -0400

    Revert "Separate into sync script"
    This reverts commit 3aa146e7a8ee5a7387e022ba5581470acbdfbee0.

--- discord-service/src/sync.ts
+++
@@ -1,6 +0,0 @@
-import { creator } from '@/app';
-
-creator.syncCommands().then(() => {
-  console.log('Slash commands synced!');
-  process.exit(0); // Immediate stop the Node process and exits with status codde 0
-});
