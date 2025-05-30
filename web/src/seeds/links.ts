d947e771948c910c8422335161a23b702bfc9872
Hong An Tran <hongantran3804@gmail.com>
2025-05-30 00:08:23 -0700

    Update links seed file

--- web/src/seeds/links.ts
+++ web/src/seeds/links.ts
@@ -3,13 +3,14 @@ import { faker } from '@faker-js/faker';
 import { CompanyName, JobPostingRegion } from '@vtmp/common/constants';
 import { LinkStatus } from '@vtmp/common/constants';
 import { formatEnumName } from '@vtmp/common/utils';
+import { JobTitle } from '@vtmp/common/constants';
 export const loadLinks = async (count: number): Promise<ILink[]> => {
   const newLinks = Array.from({ length: count }, () => ({
     url: faker.internet.url(),
     companyName: formatEnumName(faker.helpers.enumValue(CompanyName)),
+    jobTitle: formatEnumName(faker.helpers.enumValue(JobTitle)),
     datePosted: faker.date.recent({ days: 90 }),
     jobDescription: faker.lorem.paragraph(),
-    jobTitle: faker.person.jobTitle(),
     location: faker.helpers.enumValue(JobPostingRegion),
     status: faker.helpers.enumValue(LinkStatus),
   }));
