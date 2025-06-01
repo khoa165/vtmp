1233727c092f9b57ca3d48e37026ec7d37a1ec6d
dsmai <son.dmai@gmail.com>
2025-06-01 17:17:02 -0400

    Moved discordConfigSchema to packages/common

--- discord-service/src/config/env.ts
+++ discord-service/src/config/env.ts
@@ -1,26 +1,9 @@
 import dotenv from 'dotenv';
-import { z } from 'zod';
+import { discordConfigSchema } from '@vtmp/common/constants';
+import { parseEnvConfig } from '@vtmp/common/utils';
 
 dotenv.config();
 
-const configSchema = z.object({
-  DISCORD_APPLICATION_ID: z.string(),
-  DISCORD_PUBLIC_KEY: z.string(),
-  DISCORD_BOT_TOKEN: z.string(),
-  API_URL: z.string(),
-  PORT: z.preprocess(Number, z.number().positive().finite()),
-  LOGIN_EMAIL: z.string().email(),
-  LOGIN_PASSWORD: z.string(),
-});
-
 export const EnvConfig = {
-  get: () => {
-    const envs = configSchema.safeParse(process.env);
-    if (!envs.success) {
-      throw new Error(
-        'Environment variables for Discord service are not set correctly. Please check your .env file.'
-      );
-    }
-    return envs.data;
-  },
+  get: () => parseEnvConfig({ env: process.env, schema: discordConfigSchema }),
 };
