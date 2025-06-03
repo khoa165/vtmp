af2b7c5e67777b0d52ce16c100ca56fd4482356e
Hong An Tran <hongantran3804@gmail.com>
2025-06-02 17:18:10 -0700

    Remove the BadRequest class in errors.ts

--- web/src/utils/errors.ts
+++ web/src/utils/errors.ts
@@ -12,11 +12,6 @@ export abstract class ApplicationSpecificError extends Error {
     this.statusCode = statusCode;
   }
 }
-export class BadRequestError extends ApplicationSpecificError {
-  constructor(message: string, metadata: object) {
-    super(message, metadata, 400);
-  }
-}
 
 export class ResourceNotFoundError extends ApplicationSpecificError {
   constructor(message: string, metadata: object) {
