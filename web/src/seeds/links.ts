4892243290b88168fc0985daac3d0bf6a43eecfc
Hong An Tran <125112230+hongantran3804@users.noreply.github.com>
2025-06-02 22:21:53 -0700

    [FRONTEND] Admin Link Dashboard (#184)
    Co-authored-by: Khoa Le <46258781+khoa165@users.noreply.github.com>

--- web/src/seeds/links.ts
+++ web/src/seeds/links.ts
@@ -1,9 +1,18 @@
 import { ILink, LinkModel } from '@/models/link.model';
 import { faker } from '@faker-js/faker';
-
+import { CompanyName, JobPostingRegion } from '@vtmp/common/constants';
+import { LinkStatus } from '@vtmp/common/constants';
+import { formatEnumName } from '@vtmp/common/utils';
+import { JobTitle } from '@vtmp/common/constants';
 export const loadLinks = async (count: number): Promise<ILink[]> => {
   const newLinks = Array.from({ length: count }, () => ({
     url: faker.internet.url(),
+    companyName: formatEnumName(faker.helpers.enumValue(CompanyName)),
+    jobTitle: formatEnumName(faker.helpers.enumValue(JobTitle)),
+    datePosted: faker.date.recent({ days: 90 }),
+    jobDescription: faker.lorem.paragraph(),
+    location: faker.helpers.enumValue(JobPostingRegion),
+    status: faker.helpers.enumValue(LinkStatus),
   }));
 
   const links = await LinkModel.insertMany(newLinks);
