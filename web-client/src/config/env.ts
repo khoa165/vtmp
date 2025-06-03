e2b99e7af94e5484285e05643a06fd15b8d8b7e7
dsmai <son.dmai@gmail.com>
2025-06-02 22:50:08 -0400

    Rebased to main

--- web-client/src/config/env.ts
+++
@@ -1,7 +0,0 @@
-import { clientConfigSchema } from '@vtmp/common/constants';
-import { parseEnvConfig } from '@vtmp/common/utils';
-
-export const EnvConfig = {
-  get: () =>
-    parseEnvConfig({ env: import.meta.env, schema: clientConfigSchema }),
-};
