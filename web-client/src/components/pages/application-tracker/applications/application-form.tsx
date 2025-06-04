dcd7673f944cf280e6f524f6cce1b56a1452e0b1
QuangMinhNguyen27405 <quangminh27405@gmail.com>
2025-06-04 23:11:41 +0700

    Add delete message

--- web-client/src/components/pages/application-tracker/applications/application-form.tsx
+++ web-client/src/components/pages/application-tracker/applications/application-form.tsx
@@ -2,6 +2,17 @@ import { ApplicationData } from '@/components/pages/application-tracker/applicat
 import { useEffect, useState } from 'react';
 import { ApplicationFormInput } from '@/components/pages/application-tracker/applications/application-form-input';
 import { Button } from '@/components/base/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/base/alert-dialog';
 
 interface ApplicationFormProps {
   currentApplication: {
@@ -38,6 +49,8 @@ export const ApplicationForm = ({
     interest: interest,
   });
 
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   useEffect(() => {
     setApplicationForm({
       note,
@@ -46,29 +59,6 @@ export const ApplicationForm = ({
       interest,
     });
   }, [note, referrer, portalLink, interest]);
-
-  const handleSubmit = () => {
-    updateApplicationMetadataFn({
-      applicationId: applicationId,
-      body: applicationForm,
-    });
-  };
-
-  const handleCancel = () => {
-    setApplicationForm({
-      note: note,
-      referrer: referrer,
-      portalLink: portalLink,
-      interest: interest,
-    });
-  };
-
-  const handleDelete = () => {
-    deleteApplicationFn(applicationId);
-    if (onOpenChange) {
-      onOpenChange(false);
-    }
-  };
 
   return (
     <div className="py-6 rounded-xl">
@@ -121,24 +111,64 @@ export const ApplicationForm = ({
       <div className="flex justify-end gap-2 mt-6">
         <Button
           className="bg-gray-500 hover:bg-gray-600 text-foreground font-bold py-2 px-6 rounded"
-          onClick={handleDelete}
+          onClick={() => setIsDialogOpen(true)}
         >
           Delete
         </Button>
         <Button
           className="bg-red-500 hover:bg-red-600 text-foreground font-bold py-2 px-6 rounded"
           variant="secondary"
-          onClick={handleCancel}
+          onClick={() => {
+            setApplicationForm({
+              note: note,
+              referrer: referrer,
+              portalLink: portalLink,
+              interest: interest,
+            });
+          }}
         >
           Cancel
         </Button>
         <Button
           className="bg-blue-500 hover:bg-blue-600 text-foreground font-bold py-2 px-6 rounded"
-          onClick={handleSubmit}
+          onClick={() => {
+            updateApplicationMetadataFn({
+              applicationId: applicationId,
+              body: applicationForm,
+            });
+          }}
         >
           Save
         </Button>
       </div>
+      <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <AlertDialogTrigger></AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              application.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setIsDialogOpen(false)}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                deleteApplicationFn(applicationId);
+                setIsDialogOpen(false);
+                if (onOpenChange) {
+                  onOpenChange(false);
+                }
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 };
