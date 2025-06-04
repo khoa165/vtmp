baffe4632cbade405cf1aae47eea7dade0540f2f
dsmai <son.dmai@gmail.com>
2025-06-04 16:02:00 -0400

    Move config schemas back to each sub-project

--- web-client/src/config/env.ts
+++ web-client/src/config/env.ts
@@ -1,5 +1,12 @@
-import { clientConfigSchema } from '@vtmp/common/constants';
+import { z } from 'zod';
 import { parseEnvConfig } from '@vtmp/common/utils';
+
+const clientConfigSchema = z.object({
+  VITE_STATSIG_CLIENT_SDK_KEY: z.string(),
+  VITE_VTMP_2024_INTERVIEWS_CSV: z.string(),
+  VITE_VTMP_2023_INTERVIEWS_CSV: z.string(),
+  VITE_API_URL: z.string(),
+});
 
 export const EnvConfig = {
   get: () =>
--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -1,8 +1,19 @@
 import dotenv from 'dotenv';
+import { z } from 'zod';
 import { parseEnvConfig } from '@vtmp/common/utils';
-import { webConfigSchema } from '@vtmp/common/constants';
+import { Environment } from '@/constants/enums';
 
 dotenv.config();
+
+const webConfigSchema = z.object({
+  MONGO_URI: z.string(),
+  PORT: z.preprocess(Number, z.number().positive().finite()),
+  JWT_SECRET: z.string(),
+  GMAIL_EMAIL: z.string(),
+  GMAIL_APP_PASSWORD: z.string(),
+  VTMP_WEB_URL: z.string(),
+  SEED_ENV: z.nativeEnum(Environment).default(Environment.DEV),
+});
 
 export const EnvConfig = {
   get: () => parseEnvConfig({ env: process.env, schema: webConfigSchema }),
