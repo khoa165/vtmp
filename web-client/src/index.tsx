28844e160f455e2aba6fdaaf159465e391fbe3ed
Dang Son Mai <son.dmai@gmail.com>
2025-06-05 02:32:55 -0400

    Bring parse EnvConfig logic to packages/common (#221)

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -3,6 +3,7 @@ import { StatsigProvider } from 'statsig-react';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import { App } from '@/components/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { EnvConfig } from '@/config/env';
 
 const queryClient = new QueryClient();
 
@@ -10,7 +11,7 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <StatsigProvider
-    sdkKey={import.meta.env.VITE_STATSIG_CLIENT_SDK_KEY}
+    sdkKey={EnvConfig.get().VITE_STATSIG_CLIENT_SDK_KEY}
     waitForInitialization={false}
     user={{}}
   >
