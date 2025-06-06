0950b35537a3d99b19fcf11474c3a9e339698b3e
Nam Nguyen <nguy1053@msu.edu>
2025-06-05 22:59:36 -0400

    Revert "Minor change to SendInvitationResponseSchema"
    This reverts commit 90bb86f81c4b755320d90da0e1e6b5980d7fd5b4.

--- web-client/src/components/pages/admins/invitations/validation.ts
+++ web-client/src/components/pages/admins/invitations/validation.ts
@@ -2,15 +2,13 @@ import { InvitationStatus } from '@vtmp/common/constants';
 import { z } from 'zod';
 
 export const SendInvitationResponseSchema = z.object({
-  data: z
-    .object({
-      receiverEmail: z.string(),
-      sender: z.string(),
-      token: z.string(),
-      expiryDate: z.string(),
-      status: z.nativeEnum(InvitationStatus, {
-        message: 'Invalid invitation status',
-      }),
-    })
-    .nullable(),
+  data: z.object({
+    receiverEmail: z.string(),
+    sender: z.string(),
+    token: z.string(),
+    expiryDate: z.string(),
+    status: z.nativeEnum(InvitationStatus, {
+      message: 'Invalid invitation status',
+    }),
+  }),
 });
