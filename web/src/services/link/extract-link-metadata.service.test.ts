6004ad1f0d333fa25c6056d79400eb2a88aa25f0
Hong An Tran <hongantran3804@gmail.com>
2025-06-09 17:33:45 -0700

    Add test ti extract metadata service

--- web/src/services/link/extract-link-metadata.service.test.ts
+++ web/src/services/link/extract-link-metadata.service.test.ts
@@ -1,13 +1,24 @@
 import { createRetryer } from '@/helpers/link.helpers';
 import { ExtractLinkMetadataService } from '@/services/link/extract-link-metadata.service';
-import { useMongoDB } from '@/testutils/mongoDB.testutil';
-describe.skip('ExtractLinkMetaDataService', () => {
-  useMongoDB();
-  const retryFunction = createRetryer();
-  it('test', async () => {
-    const result = await retryFunction(() =>
-      ExtractLinkMetadataService.extractMetadata('https://www.indeed.com/')
-    );
-    console.log(result);
+import { wait } from 'ts-retry-promise';
+import { expect } from 'chai';
+describe('ExtractLinkMetaDataService', () => {
+  const retryFunction = createRetryer(3, 1);
+  it('should throw error when accessing forbidden website', async () => {
+    await expect(
+      retryFunction(() =>
+        ExtractLinkMetadataService.extractMetadata('https://www.indeed.com/')
+      )
+    ).eventually.rejectedWith(Error);
+  });
+  it('should throw error when the function exceed time limit', async () => {
+    await expect(
+      retryFunction(() => {
+        wait(2);
+        return ExtractLinkMetadataService.extractMetadata(
+          'https://www.indeed.com/'
+        );
+      })
+    ).eventually.rejectedWith(Error);
   });
 });
