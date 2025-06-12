6c91949ecb28bc7fa4280c8a079889cbf70904d4
dsmai <son.dmai@gmail.com>
2025-06-12 16:17:08 -0400

    Removed link processing from web

--- web/src/services/link/extract-link-metadata.service.test.ts
+++
@@ -1,9 +0,0 @@
-import { ExtractLinkMetadataService } from '@/services/link/extract-link-metadata.service';
-describe.skip('ExtractLinkMetaDataService', () => {
-  it('test', async () => {
-    const result = await ExtractLinkMetadataService.extractMetadata(
-      'https://careers.adobe.com/us/en/job/R147746/2025-Intern-Software-Engineer'
-    );
-    console.log(result);
-  });
-});
