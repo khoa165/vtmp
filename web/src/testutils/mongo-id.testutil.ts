79a75015336036aea03ade4f8834e5c85759f954
Nam Nguyen <nguy1053@msu.edu>
2025-06-12 21:19:04 -0400

    Revert codebase to main versions

--- web/src/testutils/mongo-id.testutil.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-export const getNewMongoId = () => new mongoose.Types.ObjectId().toHexString();
-export const getNewObjectId = () => new mongoose.Types.ObjectId();
-export const toMongoId = (stringId: string) =>
-  new mongoose.Types.ObjectId(stringId);
