5e761a7228e288d61037935ee4e981c52284c9ce
dsmai <son.dmai@gmail.com>
2025-06-27 02:01:05 -0400

    Added workspaceName and remove console.log

--- packages/common/src/utils/helpers.ts
+++ packages/common/src/utils/helpers.ts
@@ -14,18 +14,19 @@ export const formatEnumName = (name: string) => {
 export const parseEnvConfig = <T extends ZodObject<ZodRawShape>>({
   env,
   schema,
+  workspaceName,
 }: {
   env: NodeJS.ProcessEnv;
   schema: T;
+  workspaceName: string;
 }): z.infer<T> => {
   const envs = schema.safeParse(env);
   if (!envs.success) {
-    console.log('Problems with: ', envs.error.issues);
     const issues = envs.error.issues
       .map((issue) => `${issue.path[0]}: ${issue.message}`)
       .join(', ');
     throw new Error(
-      `Environment variables are not set correctly. Please check your .env file. Problems with: ${issues}`
+      `Environment variables are not set correctly in ${workspaceName} workspace. Please check your .env file. Problems with: ${issues}`
     );
   }
   return envs.data;
