b17f7895ef8b3509b7c159226bad6d2495153b10
Duy Kha <duykha121314888@gmail.com>
2025-06-28 00:52:54 -0500

    Pagination Controller

--- web/src/controllers/job-posting.controller.ts
+++ web/src/controllers/job-posting.controller.ts
@@ -1,10 +1,18 @@
-import { Request, Response } from 'express';
-import { z } from 'zod';
-import { JobPostingService } from '@/services/job-posting.service';
-import { JobFunction, JobPostingRegion, JobType } from '@vtmp/common/constants';
-import { getUserFromRequest } from '@/middlewares/utils';
 import { parse } from 'date-fns';
+import { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import { z } from 'zod';
+
+import {
+  JobFunction,
+  JobPostingRegion,
+  JobPostingSortField,
+  JobType,
+  SortOrder,
+} from '@vtmp/common/constants';
+
+import { getUserFromRequest } from '@/middlewares/utils';
+import { JobPostingService } from '@/services/job-posting.service';
 
 const JobPostingUpdateSchema = z
   .object({
@@ -57,6 +65,10 @@ const JobPostingIdParamSchema = z.object({
 
 const FilterSchema = z
   .object({
+    limit: z.coerce.number().int().default(10),
+    cursor: z.string().optional(),
+    sortField: z.nativeEnum(JobPostingSortField).optional(),
+    sortOrder: z.nativeEnum(SortOrder).optional(),
     jobTitle: z.string().optional(),
     companyName: z.string().optional(),
     location: z.nativeEnum(JobPostingRegion).optional(),
@@ -73,14 +85,6 @@ const FilterSchema = z
   );
 
 export const JobPostingController = {
-  getJobPostingById: async (req: Request, res: Response) => {
-    const { jobPostingId } = JobPostingIdParamSchema.parse(req.params);
-    const jobPosting = await JobPostingService.getJobPostingById(jobPostingId);
-
-    res.status(200).json({
-      data: jobPosting,
-    });
-  },
   updateJobPosting: async (req: Request, res: Response) => {
     const { jobPostingId } = JobPostingIdParamSchema.parse(req.params);
     const jobPostingData = JobPostingUpdateSchema.parse(req.body);
