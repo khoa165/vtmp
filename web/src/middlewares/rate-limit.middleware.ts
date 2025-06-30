3886a50f0a1e362779f857a56fe5c44125f70add
Hong An Tran <hongantran3804@gmail.com>
2025-06-29 21:21:44 -0700

    Update EnvConfig in rate-limit.middleware

--- web/src/middlewares/rate-limit.middleware.ts
+++ web/src/middlewares/rate-limit.middleware.ts
@@ -1,10 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
 import rateLimit from 'express-rate-limit';
 import { slowDown } from 'express-slow-down';
-import { NextFunction, Request, Response } from 'express';
+
+import { EnvConfig } from '@/config/env';
 import { handleError } from '@/utils/errors';
 
 export const rateLimitMiddleware = () => {
-  if (process.env.NODE_ENV === 'test') {
+  if (EnvConfig.get().NODE_ENV === 'TEST') {
     return (_req: Request, _res: Response, next: NextFunction) => {
       next();
     };
