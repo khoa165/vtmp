e9dfb57f1985a98ab3ee20a534a10c6e679fc3ec
QuangMinhNguyen27405 <quangminh27405@gmail.com>
2025-07-05 12:36:01 -0400

    Update interview background

--- web-client/src/components/pages/application-tracker/applications/interview-form.tsx
+++ web-client/src/components/pages/application-tracker/applications/interview-form.tsx
@@ -274,7 +274,7 @@ export const InterviewCreateForm = ({
   });
 
   return (
-    <div className="rounded-xl bg-neutral-700 border border-background p-6 mb-4 shadow-[0_8px_30px_rgba(0,0,0,0.35)] space-y-3">
+    <div className="rounded-xl bg-background border border-background p-6 mb-4 shadow-[0_8px_30px_rgba(0,0,0,0.35)] space-y-3">
       <Form {...interviewForm}>
         <div>
           <FormField
