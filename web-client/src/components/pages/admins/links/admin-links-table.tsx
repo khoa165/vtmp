d029e69741f3351e79c52b35182442bf86503283
dsmai <son.dmai@gmail.com>
2025-07-07 02:30:36 -0400

    Rename button

--- web-client/src/components/pages/admins/links/admin-links-table.tsx
+++ web-client/src/components/pages/admins/links/admin-links-table.tsx
@@ -66,7 +66,7 @@ export function AdminLinksTable<TData, TValue>({
         />
         <div className="flex items-center gap-2">
           <Button variant="outline" onClick={triggerCronFn}>
-            Run Processing Pipeline
+            Trigger Pipeline
           </Button>
           <ColumnVisibilityConfiguration table={table} />
         </div>
