50157dc4d490fa19efbabab582af944499e3a229
junphuc <phucdinhtruong@usf.edu>
2025-07-07 13:33:15 -0400

    Add filter front end (#329)
    Co-authored-by: Khoa Le <46258781+khoa165@users.noreply.github.com>

--- web-client/src/components/pages/application-tracker/job-postings/job-postings-page.tsx
+++ web-client/src/components/pages/application-tracker/job-postings/job-postings-page.tsx
@@ -12,7 +12,7 @@ export const JobPostingsPage = () => {
   }
 
   return (
-    <div className="container mx-auto py-10">
+    <div className="container p-10">
       <h1 className="text-3xl font-bold text-foreground">
         Welcome back, {`${user.firstName} ${user.lastName}`}.
       </h1>
