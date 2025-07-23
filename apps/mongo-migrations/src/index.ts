00fa74a2d27d201c156d8f3ef04c7ebbc12eff7a
Khoa Le <khoatle165@gmail.com>
2025-07-22 23:37:48 -0400

    Add script to create admins

--- apps/mongo-migrations/src/index.ts
+++ apps/mongo-migrations/src/index.ts
@@ -1,8 +1,20 @@
+import { useMongoDB } from '@vtmp/mongo/utils';
+// import mongoose from 'mongoose';
+
 import { exit } from 'process';
 
+import { createAdmins } from '@vtmp/mongo-migrations/create-admins';
+import { EnvConfig } from '@vtmp/mongo-migrations/env';
 import { migratePeople } from '@vtmp/mongo-migrations/migrate-people';
 
-migratePeople()
+const runScripts = async () => {
+  await useMongoDB(EnvConfig.get().MONGO_URI);
+  // await mongoose.connection.dropDatabase();
+  await migratePeople();
+  await createAdmins();
+};
+
+runScripts()
   .then(() => {
     console.log('Migration completed!');
     exit(0);
--- apps/mongo-migrations/src/migrate-people.ts
+++ apps/mongo-migrations/src/migrate-people.ts
@@ -1,5 +1,4 @@
 import { ProgramProfileModel, UserModel } from '@vtmp/mongo/models';
-import { useMongoDB } from '@vtmp/mongo/utils';
 import { splitFirstAndLastName } from '@vtmp/server-common/utils';
 import mongoose from 'mongoose';
 import { hasAtLeast, values } from 'remeda';
@@ -8,115 +7,106 @@ import { MentorshipRole, SystemRole } from '@vtmp/common/constants';
 import { mentorshipPeople } from '@vtmp/common/people';
 import type { MentorshipTerm } from '@vtmp/common/people';
 
-import { EnvConfig } from '@vtmp/mongo-migrations/env';
-
 export const migratePeople = async () => {
-  await useMongoDB(EnvConfig.get().MONGO_URI);
-  const client = mongoose.connection.getClient();
+  // Start a session for transactions
+  const session = await mongoose.startSession();
 
   try {
-    // Start a session for transactions
-    const session = client.startSession();
+    const people = values(mentorshipPeople);
+    const totalPeople = people.length;
 
-    try {
-      const people = values(mentorshipPeople);
-      const totalPeople = people.length;
+    console.log(`Processing ${totalPeople} people in a single batch`);
 
-      console.log(`Processing ${totalPeople} people in a single batch`);
+    await session.withTransaction(async () => {
+      // Prepare batch data
+      const userBatch = [];
+      const programProfileBatch = [];
 
-      await session.withTransaction(async () => {
-        // Prepare batch data
-        const userBatch = [];
-        const programProfileBatch = [];
-
-        for (const person of people) {
-          if (!hasAtLeast(person.terms, 1)) {
-            console.log(`Skipping ${person.name}: No mentorship term found`);
-            continue;
-          }
-
-          const { firstName, lastName } = splitFirstAndLastName(person.name);
-
-          // Prepare user data
-          userBatch.push({
-            firstName,
-            lastName,
-            email: `${firstName.toLowerCase()}.${lastName?.toLowerCase()}@mentorship.com`,
-            encryptedPassword: 'password',
-            role: SystemRole.USER,
-            activated: false,
-          });
-
-          // Prepare program profile data (will be created after users)
-          const term2025 = person.terms.find(
-            (t: MentorshipTerm) => t.year === 2025
-          );
-          const currentRoles =
-            term2025?.roles.filter(
-              (role: MentorshipRole) =>
-                ![
-                  MentorshipRole.SWE_INACTIVE_LEAD,
-                  MentorshipRole.SWE_EXMENTEE_INACTIVE_MENTOR,
-                  MentorshipRole.COMMUNITY_MEMBER,
-                ].includes(role)
-            ) ?? [];
-
-          programProfileBatch.push({
-            programName: person.name,
-            // userId will be set after user creation
-            yearJoined: person.terms[0].year,
-            isActive: hasAtLeast(currentRoles, 1),
-            hobbies: person.hobbies.split(', '),
-            school: person.school,
-            linkedin: person.linkedin,
-            currentProfessionalTitle: person.professionalTitle,
-            isFounder: person.isFounder ?? false,
-            wasMentee: !person.hasNeverBeenMenteeOfProgram,
-            wasExternallyRecruitedMentor: person.externallyRecruited ?? false,
-            spreadsheetAlias: person.trackingName,
-          });
+      for (const person of people) {
+        if (!hasAtLeast(person.terms, 1)) {
+          console.log(`Skipping ${person.name}: No mentorship term found`);
+          continue;
         }
 
-        // Create users in batch
-        if (userBatch.length > 0) {
-          const users = await UserModel.create(userBatch, {
-            session,
-            ordered: true,
-          });
-          console.log(`Created ${users.length} users in batch`);
+        const { firstName, lastName } = splitFirstAndLastName(person.name);
 
-          // Create program profiles in batch with user IDs
-          if (programProfileBatch.length > 0) {
-            const profilesWithUserIds = programProfileBatch.map(
-              (profile, index) => {
-                const user = users[index];
-                if (!user) {
-                  throw new Error(`User at index ${index} not found`);
-                }
-                return {
-                  ...profile,
-                  userId: user._id,
-                };
+        // Prepare user data
+        userBatch.push({
+          firstName,
+          lastName,
+          email: `${firstName.toLowerCase()}.${lastName?.toLowerCase()}@mentorship.com`,
+          encryptedPassword: 'password',
+          role: SystemRole.USER,
+          activated: false,
+        });
+
+        // Prepare program profile data (will be created after users)
+        const term2025 = person.terms.find(
+          (t: MentorshipTerm) => t.year === 2025
+        );
+        const currentRoles =
+          term2025?.roles.filter(
+            (role: MentorshipRole) =>
+              ![
+                MentorshipRole.SWE_INACTIVE_LEAD,
+                MentorshipRole.SWE_EXMENTEE_INACTIVE_MENTOR,
+                MentorshipRole.COMMUNITY_MEMBER,
+              ].includes(role)
+          ) ?? [];
+
+        programProfileBatch.push({
+          programName: person.name,
+          // userId will be set after user creation
+          yearJoined: person.terms[0].year,
+          isActive: hasAtLeast(currentRoles, 1),
+          hobbies: person.hobbies.split(', '),
+          school: person.school,
+          linkedin: person.linkedin,
+          currentProfessionalTitle: person.professionalTitle,
+          isFounder: person.isFounder ?? false,
+          wasMentee: !person.hasNeverBeenMenteeOfProgram,
+          wasExternallyRecruitedMentor: person.externallyRecruited ?? false,
+          spreadsheetAlias: person.trackingName,
+        });
+      }
+
+      // Create users in batch
+      if (userBatch.length > 0) {
+        const users = await UserModel.create(userBatch, {
+          session,
+          ordered: true,
+        });
+        console.log(`Created ${users.length} users in batch`);
+
+        // Create program profiles in batch with user IDs
+        if (programProfileBatch.length > 0) {
+          const profilesWithUserIds = programProfileBatch.map(
+            (profile, index) => {
+              const user = users[index];
+              if (!user) {
+                throw new Error(`User at index ${index} not found`);
               }
-            );
+              return {
+                ...profile,
+                userId: user._id,
+              };
+            }
+          );
 
-            const profiles = await ProgramProfileModel.create(
-              profilesWithUserIds,
-              { session, ordered: true }
-            );
-            console.log(`Created ${profiles.length} program profiles in batch`);
-            console.log(
-              `Successfully migrated ${profiles.length} people in a single transaction!`
-            );
-          }
+          const profiles = await ProgramProfileModel.create(
+            profilesWithUserIds,
+            { session, ordered: true }
+          );
+          console.log(`Created ${profiles.length} program profiles in batch`);
+          console.log(
+            `Successfully migrated ${profiles.length} people in a single transaction!`
+          );
         }
-      });
+      }
+    });
 
-      console.log('Finished migrating people!');
-    } finally {
-      await session.endSession();
-    }
+    console.log('Finished migrating people!');
   } finally {
-    await client.close();
+    await session.endSession();
   }
 };
